f60ead55d7fa64e21b72beede69213af71cc9d19
Yuhe <1641284022@qq.com>
2024-07-19 11:17:45 +0800

    android-mapTest

---
+++ app/android/android-mapTest/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+		<view>
+			<map id="map" :latitude="latitude" :longitude="longitude" :scale="scale"
+				:markers="markerList" style="width: 100%; height: 1000px}"></map>
+		</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				latitude: 39.915,
+				longitude: 116.404,
+				scale: 8,
+				markerList: []
+			}
+		},
+		onLoad() {
+			this.markerList.push({
+				id: 1,
+				latitude: 39.815,
+				longitude: 116.404,
+				iconPath: "/static/marker.svg",
+				callout: {
+					content: "设备编号：11111" + "\n" + 
+									"安装地址：222222"
+				}
+			})
+			console.log(this.markerList)
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
