7ebca6fcdb4f644eb95d2c25069c137c95366d8d
Yuhe <1641284022@qq.com>
2024-07-23 21:04:11 +0800

    mp-weixin-slot

---
+++ mp/mp-weixin-slot/components/child-component/child-component.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>
+		<slot :data="data"></slot>
+	</view>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+	const data = ref([1,2,3])
+</script>
+
+<style>
+
+</style>
---
+++ mp/mp-weixin-slot/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<child-component v-slot="data">
+		使用了变量 {{data}}
+	</child-component>
+	<child-component v-slot="data">
+		没有使用了变量
+	</child-component>
+	<child-component>
+		什么都没有
+	</child-component>
+</template>
+
+<script setup>
+	import { ref } from 'vue';
+	
+</script>
+
+<style>
+
+</style>
