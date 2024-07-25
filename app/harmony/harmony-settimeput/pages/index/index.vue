2e8a25beced7f02d143b3fdf9d0f9f202d8edfdb
Yuhe <1641284022@qq.com>
2024-07-25 10:23:03 +0800

    harmony-settimeout

---
+++ app/harmony/harmony-settimeput/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>  
+    <view style="text-align: center;" v-for="(value, key) in listData" :key="key">  
+        <text>{{ value }}</text>  
+    </view>  
+</template>  
+
+<script lang="ts">  
+export default {  
+    data() {  
+        return {  
+            listData: ['a', 'b', 'z']  
+        };  
+    },  
+    onLoad() {  
+        this.listData = ['a', 'b', 'z'];  
+        this.listData.push('d'); 
+        setTimeout(() => {  
+            this.listData.push('e');  
+            // this.listData.push(...['d', 'e', 'f']);  
+        }, 2000);  
+    } 
+};  
+
+// vue3响应式写法不支持呢  
+// import { ref, onMounted } from 'vue';  
+
+// const listData = ref();  
+
+// onMounted(() => {  
+//  listData.value = ['a', 'b', 'c'];  
+
+//  setTimeout(() => {  
+//      listData.value.push(...['d', 'e', 'f']);  
+//  }, 2000);  
+// });  
+</script>  
+
+<style></style>  
