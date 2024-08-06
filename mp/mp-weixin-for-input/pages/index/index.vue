54682ac099d95d0e036bb2a84f4909da989b8d28
yuhe <1641284022@qq.com>
2024-08-06 16:21:50 +0800

    mp-weixin-for-input

---
+++ mp/mp-weixin-for-input/pages/index/index.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="view">
+		<view class="view">
+			<view v-for="(item,index) in data" :key="index">
+				<input type="text" v-model="item.num" @input="input" @blur="blur"/>
+			</view>
+			<button @click="click">clogData</button>
+			<input type="text" v-model="num" @input="input" @blur="blur"/>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				num:3,
+				data:[
+					{
+						num:2
+					},{
+						num:1
+					}
+				]
+			}
+		},
+		onShow() {
+			console.log("onShow-2",getCurrentPages())
+			// uni.setTabBarItem({
+			//  index: 0,  
+			//  text: 'yuhe'  
+			// })  
+		},
+		methods: {
+			click(){
+				console.log(this.data)
+			},
+			input(r){
+				console.log(r)
+			},
+			blur(){
+				
+			}
+		}
+	}
+</script>
+
+<style>
+	.view{
+		margin-top: 150rpx;
+	}
+</style>
