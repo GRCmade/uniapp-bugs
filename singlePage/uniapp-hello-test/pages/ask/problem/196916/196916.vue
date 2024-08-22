0fc20b99a439c50973324a4d79f1064aa2961c6c
yuhe <1641284022@qq.com>
2024-08-22 22:08:51 +0800

    8.22

--- singlePage/uniapp-hello-test/pages/ask/problem/196916/196916.vue
+++ singlePage/uniapp-hello-test/pages/ask/problem/196916/196916.vue
@@ -1,26 +1,60 @@
 <template>
-	<view>
-		<uni-number-box @change="change" @blur="blur" :min="1" :max="999" background="transparent" v-model="goodsCount" />
+	<view class="page">
+		<uni-card :is-shadow="false" is-full>
+			<text class="uni-h6">数字输入框组件多用于购物车加减商品等场景</text>
+		</uni-card>
+		<uni-section title="基本用法" type="line" padding>
+			<uni-number-box @change="changeValue" />
+		</uni-section>
+		<uni-section :title="'使用v-model : '+ vModelValue" subTitle="使用 v-model 显示默认值" type="line" padding>
+			<uni-number-box v-model="vModelValue" @blur="blur" @focus="focus" @change="changeValue" />
+		</uni-section>
+		<uni-section title="设置最小值和最大值" subTitle="使用 min \ max 属性设置最大最小值" type="line" padding>
+			<uni-number-box :min="2" :max="9" :value="555" />
+		</uni-section>
+		<uni-section title="设置步长（步长0.1)" subTitle="使用 step 属性设置步长" type="line" padding>
+			<uni-number-box :value="1.1" :step="0.1" />
+		</uni-section>
+		<uni-section title="自定义背景" type="line" subTitle="使用 background 属性设置自定义背景色" padding>
+			<uni-number-box :value="50" background="#2979FF" color="#fff" />
+		</uni-section>
+		<uni-section title="禁用状态" subTitle="使用 disabled 属性设置组件禁用" type="line" padding>
+			<uni-number-box :disabled="true" />
+		</uni-section>
+		<uni-section :title="'获取输入的值 : '+ numberValue" type="line" padding>
+			<uni-number-box :value="numberValue" @change="change" />
+		</uni-section>
 	</view>
 </template>
 
+
 <script>
 	export default {
+		components: {},
 		data() {
 			return {
-				goodsCount:0
+				numberValue: 0,
+				vModelValue: 3
 			}
 		},
 		methods: {
-			change(e){
-				console.log("change",e)
+			change(value) {
+				this.numberValue = value
 			},
-			blur(e){
-				console.log("blur",e)
+			changeValue(value) {
+				console.log('返回数值：', value);
+			},
+			blur(e) {
+				console.log('blur:', e);
+			},
+			focus(e) {
+				console.log('focus:', e);
 			}
+
 		}
 	}
 </script>
+
 
 <style>
 
---
+++ singlePage/uniapp-hello-test/pages/ask/problem/197042/197042-1.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		197042-1
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad(e) {
+			console.log("197042-1",e)
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
