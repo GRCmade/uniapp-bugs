047aa59edd7a11583c4be3e744bac76898bee1da
yuhe <1641284022@qq.com>
2024-09-09 20:27:50 +0800

    raycast auto input

---
+++ singlePage/uniapp-hello-test/pages/ask/problem/198027/198027.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<!-- <uni-number-box :min="parseFloat(currentNJ.rowSoacing)" :step="0.1" v-model="rwdata.rowSoacing" /> -->
+		<uni-number-box :min="parseFloat(min)" :step="0.1" v-model="num" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				num:'',
+				min:0
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
