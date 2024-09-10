cb5ef1df91b00cce886acaf66585f898794af84e
yuhe <1641284022@qq.com>
2024-09-10 19:58:28 +0800

    raycast auto input

---
+++ singlePage/uniapp-hello-test/pages/ask/problem/198084/198084.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<button @click="click">click</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			click(){
+				uni.reLaunch({
+					url:"/pages/ask/index"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
