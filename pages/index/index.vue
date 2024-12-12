bac2e94f55e36c993cb1d08835fdd3c3f9a4052d
愈合 <1641284022@qq.com>
2024-12-12 20:38:35 +0800

    测试各种格式的路径

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <view>
    <button @click="click">click</button>
+   <button @click="click2">click2</button>
   </view>
 </template>
 
@@ -15,14 +16,24 @@ export default {
 			uni.setTabBarItem({
 			  index: 1,
 			  text: 'yuhe',
-			  iconPath: '/static/tabbar/1.png',
+			  // iconPath: '/static/tabbar/1.png',
+			  iconPath: '../../static/img1/image/2.png',
 			  selectedIconPath: '/static/tabbar/11.png'
 			})
-    }
+    },
+		click2(){
+			uni.setTabBarStyle({
+			  color: '#FF0000',
+			  selectedColor: '#00FF00',
+			  backgroundImage:"/static/back.png"
+			})
+
+		}
   }
 }
 </script>
 
 
 <style></style>
+
 
