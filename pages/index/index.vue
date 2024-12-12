3115d041faa7a105eaf3540f2c4579ec0b465614
愈合 <1641284022@qq.com>
2024-12-12 14:55:15 +0800

    新建一个 template

---
+++ pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+   <button @click="click">click</button>
+  </view>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    click(){
+
+    }
+  }
+}
+</script>
+
+
+<style></style>
+
