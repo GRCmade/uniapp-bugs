9246243c8129751926c3e7e763401dd4c55ee99d
愈合 <1641284022@qq.com>
2024-12-13 16:51:17 +0800

    cli 模板

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,48 +1,23 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+  <view>
+   <button @click="click">click 12</button>
   </view>
 </template>
+
 
 <script>
 export default {
   data() {
-    return {
-      title: 'Hello',
-    }
+    return {}
   },
-  onLoad() {},
-  methods: {},
+  methods: {
+    click(){
+			
+    }
+  }
 }
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+<style></style>
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
