6376ee168a8d9ab1a2be8041e821f55784cb9209
愈合 <1641284022@qq.com>
2024-12-13 12:02:11 +0800

    修改测试示例

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,39 +1,38 @@
 <template>
-  <view>
-   <button @click="click">click</button>
-   <button @click="click2">click2</button>
-  </view>
+	<view>
+		<view v-for="(item,index) in ls" :key="index">
+			<button @click="click(item)">{{item}}</button>
+			<image :src="item" mode="aspectFit"></image>
+		</view>
+	</view>
 </template>
 
 
 <script>
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    click(){
-			uni.setTabBarItem({
-			  index: 1,
-			  text: 'yuhe',
-			  // iconPath: '/static/tabbar/1.png',
-			  iconPath: '../../static/img1/image/2.png',
-			  selectedIconPath: '/static/tabbar/11.png'
-			})
-    },
-		click2(){
-			uni.setTabBarStyle({
-			  color: '#FF0000',
-			  selectedColor: '#00FF00',
-			  backgroundImage:"/static/back.png"
-			})
-
+	export default {
+		data() {
+			return {
+				ls: ["/static/1..png", "static/2..png", "./static/3..png", "../static/4..png", "../../static/5..png","6..png"]
+			}
+		},
+		methods: {
+			click(item) {
+				uni.setTabBarItem({
+					index: 1,
+					text: 'yuhe',
+					// iconPath: '/static/tabbar/1.png',
+					iconPath: item,
+					selectedIconPath: '/static/tabbar/11.png'
+				})
+			},
 		}
-  }
-}
+	}
 </script>
 
 
-<style></style>
-
-
+<style>
+	image {
+		width: 50px;
+		height: 50px;
+	}
+</style>
