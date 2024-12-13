e624e38dd9bb42f27e993b72ba8603063aa4771c
愈合 <1641284022@qq.com>
2024-12-13 11:33:08 +0800

    harmony-canvasGetImageData

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <view>
    <button @click="click">click</button>
+	 <canvas canvas-id="myCanvas"></canvas>
   </view>
 </template>
 
@@ -12,7 +13,20 @@ export default {
   },
   methods: {
     click(){
-			
+			uni.canvasGetImageData({
+			  canvasId: 'myCanvas',
+			  x: 0,
+			  y: 0,
+			  width: 100,
+			  height: 100,
+			  success(res) {
+					console.log(JSON.stringify(res))
+			    // console.log(res.width) // 100
+			    // console.log(res.height) // 100
+			    // console.log(res.data instanceof Uint8ClampedArray) // true
+			    // console.log(res.data.length) // 100 * 100 * 4
+			  }
+			})
     }
   }
 }
