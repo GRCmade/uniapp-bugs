b5c9cc7a1b3666a786ae698c38b72a11c999b00d
愈合 <1641284022@qq.com>
2024-12-13 16:19:44 +0800

    harmony-JSONparse

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -12,16 +12,24 @@
 		},
 		methods: {
 			click() {
-				const data = "{\"key\":\"8aee060599e0221f32a03d99f7f31d86\",\"policyName\":\"原价\",\"showSeats\":[{\"salePrice\":300,\"snapshot\":\"十七区/3排20号\",\"fareLevel\":\"D\",\"suitCode\":null,\"id\":\"1858723193608245249\",\"price\":300,\"fareLevelVerboseName\":\"乙级票\"}],\"totalAmount\":300,\"deltaPrice\":0,\"buyProtocolOn\":false,\"buyProtocol\":\"Empty_String\",\"imageUrl\":\"https://res.dasheng.top/227/pro/ctms_tool/20241208/jpg/cebd003085e94068bfad67db82ba752a.jpg\",\"name\":\"【南京】2024-2025赛季CBA联赛常规赛南京赛区赛事\",\"venueAddress\":\"江苏省南京市鼓楼区华侨路街道双沟珍宝坊体育馆\",\"realNameType\":\"3\",\"id\":\"1838480455383248898\",\"performanceSource\":null,\"showTime\":\"2024-12-18 19:35:00\",\"showName\":\"2024-12-18 19:35 南京头排苏酒 VS 广东东阳光\",\"showId\":\"1858723021008441345\",\"douyinProductId\":\"7421175268624140326\",\"ticketOutletId\":\"\",\"plugInType\":1}";
-				console.log("data",data)
-				let options = {}
-				options = JSON.parse(data)
-				// try {
-				// 	options = JSON.parse(data)
-				// } catch (error) {
-				// 	//TODO handle the exception
-				// }
-				console.log(options)
+				uni.request({
+					url:"http://192.168.31.148:3000/api/data",
+					header:{
+						"content-type": "text/plain"
+					},
+					success(res) {
+						console.log(res.data.slice(0,-1))
+						try {
+							const str = res.data.slice(0,-1)
+							const obj = JSON.parse(str)
+							console.log(obj)
+						} catch (error) {
+							console.log(error)
+							//TODO handle the exception
+						}
+
+					}
+				})
 			}
 		}
 	}
