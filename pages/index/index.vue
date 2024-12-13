fe496490559cd55333186ec4f4c284cae7585a5e
愈合 <1641284022@qq.com>
2024-12-13 15:17:52 +0800

    harmony-setStorageSync

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
   <view>
-   <button @click="click">click</button>
+   <button @click="setStorage">setStorage</button>
+   <button @click="getStorage">getStorage</button>
   </view>
 </template>
 
@@ -11,9 +12,22 @@ export default {
     return {}
   },
   methods: {
-    click(){
-			
-    }
+    setStorage(){
+			console.log("setStorage")
+			const user = {
+				orgName : "dcloudio",
+				position : ".net工程师"
+			}
+			console.log(JSON.stringify(user))
+			uni.setStorageSync('userInfo',user)
+    },
+		getStorage(){
+			console.log("getStorage")
+			var userInfo = uni.getStorageSync('userInfo')
+			userInfo.position = ".js工程师"
+			uni.setStorageSync('userInfo',userInfo)
+			console.log(JSON.stringify(userInfo))
+		}
   }
 }
 </script>
