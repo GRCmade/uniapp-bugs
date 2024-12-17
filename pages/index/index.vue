f82ded7cd5ac2c21e2006396838c687e5fe9fcd3
愈合 <1641284022@qq.com>
2024-12-17 19:38:29 +0800

    新加入腾讯地图相关的内容

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,42 +1,22 @@
 <template>
 	<view>
-		<view class="page-body">
-			<view class="page-section page-section-gap">
-				<map style="width: 100%; height: 300px;" :latitude="latitude" :longitude="longitude" :markers="covers">
-				</map>
-			</view>
-		</view>
+		<button @click="click">click</button>
 	</view>
 </template>
 
 
-
 <script>
-export default {
-	data() {
-		return {
-			id:0, // 使用 marker点击事件 需要填写id
-			title: 'map',
-			latitude: 39.909,
-			longitude: 116.39742,
-			covers: [{
-				latitude: 39.909,
-				longitude: 116.39742,
-				iconPath: '/static/logo.png'
-			}, {
-				latitude: 39.90,
-				longitude: 116.39,
-				iconPath: '/static/logo.png'
-			}]
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			click() {
+				
+			}
 		}
-	},
-	methods: {
-
 	}
-}
 </script>
 
 
-
 <style></style>
-
