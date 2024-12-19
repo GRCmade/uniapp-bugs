b59c61fd72123deaeb07d7c71f7492c17c28b104
愈合 <1641284022@qq.com>
2024-12-19 17:51:20 +0800

    harmony-uts-arraybuffer

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,22 +1,65 @@
 <template>
-	<view>
-		<button @click="click">click</button>
+	<view class="content">
+		<button style="margin-top: 100px;" class="button"></button>
+		<button class="button" @click="chooseImage">chooseImage</button>
 	</view>
 </template>
 
-
-<script>
+<script lang="uts">
+	
+	import { hsReadFile } from '@/uni_modules/srh-readFile';
+	
 	export default {
 		data() {
-			return {}
+			return {
+
+			}
+		},
+		onLoad() {
+
 		},
 		methods: {
-			click() {
+			chooseImage() {
+				uni.chooseImage({
+				    count: 1, 
+					sizeType: ['original', 'compressed'],
+				    sourceType: ['album'],
+				    success: async res => {
+						console.log("res" , res)
+						let tempFilePath = res.tempFilePaths[0];
+						
+						uni.saveFile({
+						    tempFilePath: tempFilePath,
+						    success: (res) => {
+								console.log('res.savedFilePath',res.savedFilePath)
+								hsReadFile(res.savedFilePath,(res)=>{
+									console.log('res.savedFilePath',res)
+								});				
+						    },
+							fail:(e) => {
+								console.log(e)
+								
+							}
+						})
 				
+				    },
+					
+				});
 			}
+			
 		}
 	}
 </script>
 
-
-<style></style>
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.button{
+		width: 100%;
+		margin: 10px;
+	}
+</style>
