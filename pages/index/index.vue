6d327dce5730b230ed0f8d27dc360ad37069ea8e
愈合 <1641284022@qq.com>
2024-12-20 12:01:09 +0800

    harmony-scroll-stop-prevent

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,18 @@
 <template>
 	<view>
+		<uni-popup ref="popup" type="top">
+			<view class="box" @touchmove.stop.prevent>
+				<scroll-view scroll-y="true" class="scroll" :scroll-top="0">
+					<view v-for="(item,index) in 100" :key="index">
+						{{item}}
+					</view>
+				</scroll-view>
+			</view>
+		</uni-popup>
 		<button @click="click">click</button>
+		<view class="view2" v-for="(item,index) in 100" :key="index">
+			{{item}}
+		</view>
 	</view>
 </template>
 
@@ -10,13 +22,29 @@
 		data() {
 			return {}
 		},
+		onLoad() {},
 		methods: {
 			click() {
-				
+				this.$refs.popup.open()
 			}
 		}
 	}
 </script>
 
 
-<style></style>
+<style>
+	.scroll {
+		height: 400px;
+		width: 100%;
+	}
+
+	.scroll view {
+		background-color: red;
+		margin: 10px;
+	}
+	
+	.view2 {
+		background-color: blue;
+		margin: 10px;
+	}
+</style>
