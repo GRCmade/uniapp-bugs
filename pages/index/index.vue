0bd8496055fdc5b85517c7e3c2bd1ab523bfc0c3
愈合 <1641284022@qq.com>
2024-12-23 17:46:44 +0800

    harmony-auth-login

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,22 +1,38 @@
 <template>
-	<view>
-		<button @click="click">click</button>
-	</view>
+  <view>
+    <button @click="getProviderSync">getProviderSync</button>
+    <button @click="login">login</button>
+    <button @click="getUserInfo">getUserInfo</button>
+  </view>
 </template>
+<script setup>
+const getProviderSync = () => {
+  const provider = uni.getProviderSync({service: 'oauth'})
+  console.log('provider :>> ' + JSON.stringify(provider));
+}
 
+const login = () => {
+  uni.login({
+    provider: 'huawei',
+    success(res) {
+      console.log(JSON.stringify(res))
+    },
+    fail(err) {
+      console.log(JSON.stringify(err))
+    },
+  })
+}
 
-<script>
-	export default {
-		data() {
-			return {}
-		},
-		methods: {
-			click() {
-				
-			}
-		}
-	}
+const getUserInfo = () => {
+  uni.getUserInfo({
+    provider: 'huawei',
+    success(res) {
+      console.log(JSON.stringify(res))
+    },
+    fail(err) {
+      console.log(JSON.stringify(err))
+    },
+  })
+}
 </script>
 
-
-<style></style>
