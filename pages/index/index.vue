88c17eb0f9be6403e2222b9aed37b5201fdadc85
愈合 <1641284022@qq.com>
2024-12-27 18:04:42 +0800

    harmony-socket

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -12,7 +12,32 @@
 		},
 		methods: {
 			click() {
-				
+				var socketOpen = false;
+				var socketMsgQueue = ["1","2"];
+
+				uni.connectSocket({
+					url: "http://192.168.31.148:3000/connection", // 替换为您的 WebSocket 服务器 URL  
+				});
+				console.log('连接HMsocket:connectSocket');
+				uni.onSocketOpen(function (res) {
+				  socketOpen = true;
+				  for (var i = 0; i < socketMsgQueue.length; i++) {
+				    sendSocketMessage(socketMsgQueue[i]);
+				  }
+				  socketMsgQueue = [];
+				});
+				uni.onSocketError(function(res) {
+					console.log('WebSocket连接打开失败，请检查！');
+				});
+				function sendSocketMessage(msg) {
+				  if (socketOpen) {
+				    uni.sendSocketMessage({
+				      data: msg
+				    });
+				  } else {
+				    socketMsgQueue.push(msg);
+				  }
+				}
 			}
 		}
 	}
