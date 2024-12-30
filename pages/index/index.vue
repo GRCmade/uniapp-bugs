678073b3ce70490c2565943e00ea4ba00260a628
愈合 <1641284022@qq.com>
2024-12-30 14:55:22 +0800

    harmony-saveImageToPhotosAlbum

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,32 @@
 <template>
-	<view>
-		<button @click="click">click</button>
-	</view>
+  <view>
+    <button @click="click">click2</button>
+  </view>
 </template>
 
 
 <script>
-	export default {
-		data() {
-			return {}
-		},
-		methods: {
-			click() {
-				
-			}
-		}
-	}
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    click() {
+      uni.chooseImage({
+        count: 1,
+        sourceType: ['camera','album'],
+        success: function (res) {
+          uni.saveImageToPhotosAlbum({
+            filePath: res.tempFilePaths[0],
+            success: function () {
+              console.log('save success');
+            }
+          });
+        }
+      });
+    }
+  }
+}
 </script>
 
 
