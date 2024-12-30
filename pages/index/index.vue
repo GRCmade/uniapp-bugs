9f0633bb6f7e70a35c8a8c4f75b791561a213a92
愈合 <1641284022@qq.com>
2024-12-30 18:03:29 +0800

    新增

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,32 +1,69 @@
 <template>
-  <view>
-    <button @click="click">click2</button>
-  </view>
+	<view>
+		<button @click="click">click 压缩</button>
+		<button @click="click2">click 原图</button>
+		<button @click="click3">click saveVideoToPhotosAlbum</button>
+	</view>
 </template>
 
 
 <script>
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    click() {
-      uni.chooseImage({
-        count: 1,
-        sourceType: ['camera','album'],
-        success: function (res) {
-          uni.saveImageToPhotosAlbum({
-            filePath: res.tempFilePaths[0],
-            success: function () {
-              console.log('save success');
-            }
-          });
-        }
-      });
-    }
-  }
-}
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			click() {
+				uni.chooseImage({
+					count: 1,
+					sourceType: ['camera', 'album'],
+					sizeType: ['compressed'],
+					success: function(res) {
+						uni.saveImageToPhotosAlbum({
+							filePath: res.tempFilePaths[0],
+							success: function() {
+								console.log('save success');
+							},
+							fail(res) {
+								console.log('save fail', JSON.stringify(res));
+							}
+						});
+					}
+				});
+			},
+			click2() {
+				uni.chooseImage({
+					count: 1,
+					sourceType: ['camera', 'album'],
+					sizeType: ['original'],
+					success: function(res) {
+						uni.saveImageToPhotosAlbum({
+							filePath: res.tempFilePaths[0],
+							success: function() {
+								console.log('save success');
+							},
+							fail(res) {
+								console.log('save fail', JSON.stringify(res));
+							}
+						});
+					}
+				});
+			},
+			click3() {
+				uni.chooseVideo({
+					sourceType: ['album', 'camera'],
+					success: function(res) {
+						uni.saveVideoToPhotosAlbum({
+							filePath: res.tempFilePath,
+							success: function() {
+								console.log('save success');
+							}
+						});
+					}
+				});
+			}
+		}
+	}
 </script>
 
 
