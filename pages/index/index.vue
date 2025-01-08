ac829bb028aaa1202e62310328f364a59b27d4d3
愈合 <1641284022@qq.com>
2025-01-08 11:52:56 +0800

    task - uni

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<view>
-		<button @click="connectSocket">连接</button>
+		<button @click="connectSocket">连接 task</button>
+		<button @click="connectSocket2">连接 uni.socket</button>
 		<button @click="sendMessage">发送</button>
 	</view>
 </template>
@@ -38,38 +39,58 @@
 			},
 			// 点击连接按钮时调用
 			connectSocket() {
+				let socketTask = uni.connectSocket({
+					url: "ws://192.168.31.148:3000/connection", // 替换为您的 WebSocket 服务器 URL
+					complete: ()=> {}
+				});
+				
+				socketTask.onOpen((data)=>{
+					console.log("socketTask.onOpen",JSON.stringify(data));
+					socketTask.send({
+						data:"socketTask.send",
+						success(res) {
+							console.log("socketTask.send",JSON.stringify(res));
+						},
+						fail(res) {
+							console.log("socketTask.send-fail",JSON.stringify(res));
+						}
+					})
+				})
+				
+			},
+			connectSocket2(){
 				uni.connectSocket({
 					url: "ws://192.168.31.148:3000/connection", // 替换为您的 WebSocket 服务器 URL
+					complete: ()=> {}
 				});
-
 				// WebSocket 连接成功
 				uni.onSocketOpen(() => {
 					console.log("WebSocket 连接已打开");
 					this.socketOpen = true;
-
+				
 					// 发送队列中的消息
 					while (this.socketMsgQueue.length > 0) {
 						const message = this.socketMsgQueue.shift();
 						this.sendSocketMessage(message);
 					}
 				});
-
+				
 				// WebSocket 连接失败
 				uni.onSocketError((res) => {
 					console.error("WebSocket 连接失败:", res);
 				});
-
+				
 				// WebSocket 收到消息
 				uni.onSocketMessage((res) => {
 					console.log("收到消息:", res.data);
 				});
-
+				
 				// WebSocket 关闭
 				uni.onSocketClose(() => {
 					console.log("WebSocket 已关闭");
 					this.socketOpen = false;
 				});
-			},
+			}
 		},
 	};
 </script>
