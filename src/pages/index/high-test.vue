dc7acdbd26764e0c1ff6358456a8fe54ca9e2da8
愈合 <1641284022@qq.com>
2025-01-08 11:52:43 +0800

    kuaishou-ref-cli

---
+++ src/pages/index/high-test.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="h_wrapper">
+
+    <view class="h_header">
+      <text>测试内容</text>
+      <text>测试内容</text>
+      <text>测试内容</text>
+      <text>测试内容</text>
+    </view>
+
+    <view class="h_body"></view>
+
+    <view class="h_footer"></view>
+
+  </view>
+</template>
+
+<script setup>
+  const open = () => {
+    console.log('i m fine')
+  }
+
+  defineExpose({
+    open
+  })
+</script>
+<style>
+  .h_wrapper {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ src/pages/index/some-test.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="h_wrapper">
+
+    <view class="h_header">
+      <text>{{ '---' + props.content + '---' }}</text>
+    </view>
+
+    <view class="h_body"></view>
+
+    <view class="h_footer"></view>
+
+  </view>
+</template>
+
+<script setup>
+  const props = defineProps({
+    content: String
+  }) 
+
+  const open = () => {
+    console.log('i m fine')
+  }
+
+  
+
+  defineExpose({
+    open
+  })
+</script>
+<style>
+  .h_wrapper {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
