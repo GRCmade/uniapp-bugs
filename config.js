c7e4fa276d07299c703c7432e24fe427a4e9c1d2
愈合 <1641284022@qq.com>
2025-01-13 11:02:21 +0800

    script init

---
+++ config.js
@@ -0,0 +1,7 @@
+export const Repository = {
+  next:"/Users/gaoruicheng/Documents/DcloudProject/uni-app-next",
+  dev:"/Users/gaoruicheng/Documents/DcloudProject/uni-app-dev",
+  now_vue3:"/Users/gaoruicheng/Documents/MyProject/uniapp-bugs/uniapp-bugs-now-cli",
+  now_vue2:"/Users/gaoruicheng/Documents/MyProject/uniapp-bugs/uniapp-bugs-now-cli-vue2",
+  hbuilderx_alpha:"/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniapp-cli-vite/node_modules/@dcloudio/uni-mp-weixin"
+}
