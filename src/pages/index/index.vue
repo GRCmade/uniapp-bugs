910809038be0218eebef4ba633c8d6d499a9d198
愈合 <1641284022@qq.com>
2025-01-13 15:44:14 +0800

    systeminfo-cli

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,23 +1,42 @@
 <template>
-  <view>
-   <button @click="click">click</button>
-  </view>
+	<view>
+		<button @click="caniuse">caniuse</button>
+		<button @click="getAppBaseInfo">getAppBaseInfo</button>
+		<button @click="getWindowInfo">getWindowInfo</button>
+		<button @click="getDeviceInfo">getDeviceInfo</button>
+		<button @click="getAppAuthorizeSetting">getAppAuthorizeSetting</button>
+		<button @click="getSystemInfoSync">getSystemInfoSync</button>
+	</view>
 </template>
 
 
 <script>
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    click(){
-			
-    }
-  }
-}
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			caniuse(){
+				// console.log(uni.canIUse());
+			},
+			getAppAuthorizeSetting(){
+				console.log(uni.getAppAuthorizeSetting())
+			},
+			getWindowInfo(){
+				console.log(uni.getWindowInfo())
+			},
+			getDeviceInfo(){
+				console.log(uni.getDeviceInfo());
+			},
+			getAppBaseInfo() {
+				console.log(uni.getAppBaseInfo())
+			},
+			getSystemInfoSync(){
+				console.log(uni.getSystemInfoSync())
+			}
+		}
+	}
 </script>
 
 
 <style></style>
-
