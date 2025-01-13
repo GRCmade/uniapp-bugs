b8c855043f647f83baf86352bcdbce9168c7b221
愈合 <1641284022@qq.com>
2025-01-13 19:05:08 +0800

    新增小程序脚本

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
-export const Repository = {
+
+const Repository = {
   next:"/Users/gaoruicheng/Documents/DcloudProject/uni-app-next",
   dev:"/Users/gaoruicheng/Documents/DcloudProject/uni-app-dev",
   now_vue3:"/Users/gaoruicheng/Documents/MyProject/uniapp-bugs/uniapp-bugs-now-cli",
   now_vue2:"/Users/gaoruicheng/Documents/MyProject/uniapp-bugs/uniapp-bugs-now-cli-vue2",
-  hbuilderx_alpha:"/Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/uniapp-cli-vite/node_modules/@dcloudio/uni-mp-weixin"
+  hbuilderx_alpha:"/Applications/HBuilderX-Alpha.app/Contents/HBuilderX",
+  hbuilderx_vue3_path:"/plugins/uniapp-cli-vite/node_modules/@dcloudio",
+  hbuilderx_vue2_path:"/plugins/uniapp-cli/node_modules/@dcloudio"
+}
+
+module.exports = {
+  Repository
 }
