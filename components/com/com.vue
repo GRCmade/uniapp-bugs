15ea159bfc0aea09eec10aa2f0be60f794b70b8b
愈合 <1641284022@qq.com>
2025-01-15 16:46:34 +0800

    harmony-uni-swiper-action

---
+++ components/com/com.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<view class="box" @touchstart="touchstart" @touchmove="touchmove"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"com",
+		data() {
+			return {
+				touchstart(){
+					console.log("touchs");
+				},
+				touchmove(){
+					console.log("touchm");
+				},
+			};
+		}
+	}
+</script>
+
+<style>
+	.box{
+		width: 600rpx;
+		height: 500rpx;
+		background-color: #36d;
+	}
+</style>
