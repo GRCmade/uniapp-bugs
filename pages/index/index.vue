1389ec19953dcbbd6e08f7da6f4f0a5b3e72cc34
愈合 <1641284022@qq.com>
2025-02-08 15:11:16 +0800

    kuaishou-ref

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,36 @@
 <template>
-	<view>
-		<button @click="click">click</button>
-	</view>
+  <view class="content">
+    <SomeTest ref="someRef" :content="someText"></SomeTest>
+    <HighTest ref="testRef"></HighTest>
+    <button @click="onButton">点击获取ref</button>
+  </view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {}
-		},
-		methods: {
-			click() {
-				
-			}
-		}
-	}
+<script setup lang="ts">
+import { ref } from 'vue'
+import SomeTest from './some-test.vue'
+import HighTest from './high-test.vue'
+
+const someText = ref('我是父组件内的内容！')
+
+const someRef = ref(null)
+const testRef = ref(null)
+
+const onButton = () => {
+  console.log('---czm someRef =', someRef.value)    // 这里打印一直是 null
+  someRef.value?.open()
+  console.log('----czm testRef = ', testRef.value)  // 可以正常获取到组件
+  testRef.value?.open()
+}
+
 </script>
 
-
-<style></style>
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+</style>
