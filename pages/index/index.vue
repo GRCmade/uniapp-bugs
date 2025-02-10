7fd48dc84c02edb240959325e0df293c54682be2
愈合 <1641284022@qq.com>
2025-02-10 15:33:44 +0800

    更新示例

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,6 +10,12 @@
 		<picker mode="date" fields="day">
 			<view class="uni-input">fields="day"</view>
 		</picker>
+		<picker mode="date" fields="day" :value="date">
+			<view class="uni-input">fields="day" && value2 = {{date}}</view>
+		</picker>
+		<picker mode="date" fields="day" :value="123">
+			<view class="uni-input">fields="day" && value2 = 123</view>
+		</picker>
 	</view>
 </template>
 
