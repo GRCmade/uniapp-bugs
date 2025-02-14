9beeeea704d0fe484d9194a86684762c9fe991f3
愈合 <1641284022@qq.com>
2025-02-14 11:55:28 +0800

    harmony-image-tempfilepath

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 ======== README.md ========
 
-# uniapp-bugs 的 template 模板
+# 鸿蒙上 image 组件不能加载临时路径
 > uniapp-bugs https://github.com/GRCmade/uniapp-bugs
 
 # 问题
+
+问题是临时地址，不能显示在页面上
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/undefined20250214110652697.png)
+
+渲染使用的是 background-image
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/undefined20250214113402956.png)
 
 ======== END ========
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,30 @@
 <template>
 	<view>
 		<button @click="click">click</button>
+		{{src}}
+		<image :src="src" mode=""></image>
 	</view>
 </template>
 
 <script>
 	export default {
 		data() {
-			return {}
+			return {
+				src: '/data/storage/el2/base/temp/HBuilder/compress/1739502265790_0.png'
+			}
 		},
 		methods: {
 			click() {
-				
+				var _this = this
+				uni.chooseImage({
+					count: 1, //默认9
+					sizeType: ['original'], //可以指定是原图还是压缩图，默认二者都有
+					sourceType: ['album'], //从相册选择
+					success: function(res) {
+						console.log(JSON.stringify(res.tempFilePaths));
+						_this.src = res.tempFilePaths[0]
+					}
+				});
 			}
 		}
 	}
