8da3848e7cf2abc5dfe9724664d2312d6c06c699
愈合 <1641284022@qq.com>
2025-02-25 19:54:26 +0800

    weixin-dom-click

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 ======== README.md ========
 
-# uniapp-bugs 的 template 模板
+# Vue3版本dom变化导致事件回调错乱 
 > uniapp-bugs https://github.com/GRCmade/uniapp-bugs
 
 # 问题
+
+快速点击正常应该触发CB2 事件
+
+但是会突然出现CB3 事件
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250225195215490.png)
+
+产生的原因：存在一个dom更新的函数导致的
+
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250225195226861.png)
 
 ======== END ========
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,21 +1,48 @@
 <template>
-	<view>
-		<button @click="click">click</button>
-	</view>
+	<div class="invisible" v-if="C1" @click="CB1"></div>
+	<div class="button" @click="CB2">{{text}}</div>
+	<div class="invisible" @click="CB3">CB3</div>
 </template>
-
 <script>
 	export default {
 		data() {
-			return {}
+			return {
+				text: '快速点击',
+				C1: true,
+			};
+		},
+		mounted() {
+			setInterval(() => {
+				this.C1 = !this.C1;
+			}, 300);
 		},
 		methods: {
-			click() {
-				
-			}
-		}
-	}
+			CB1() {},
+			CB2() {
+				this.text = '正确: 触发了CB2';
+				console.log(this.text);
+			},
+			CB3() {
+				this.text = '错误: 触发了CB3';
+				console.log(this.text);
+			},
+		},
+	};
 </script>
+<style lang="less">
+	.invisible {
+		height: 0px;
+	}
 
-
-<style></style>
+	.button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+		font-size: 80px;
+		color: white;
+		;
+		background-color: red;
+	}
+</style>
