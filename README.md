11270ca276186f6b37234c047c44c56a8373c8d7
愈合 <1641284022@qq.com>
2025-02-26 20:16:37 +0800

    harmony-downloadfile-header

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 ======== README.md ========
 
-# uniapp-bugs 的 template 模板
+# 鸿蒙上downloadFile 下载文件的文件重命名
 > uniapp-bugs https://github.com/GRCmade/uniapp-bugs
 
 # 问题
+downloadFile 这个接口没办法解析content-disposition里的文件名，用的是时间戳的文件名，后缀解析有误，那个正则应该是多了双引号，这个能否优先处理一下@scanf 
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250226200807309.png)
+
+
+记录一个浏览器获取文件的地方：
+- url 可以获取文件的名称和后缀
+- contentDisposition 可以获取文件的后缀
+- 服务器返回的 请求头中的 fileName 可以获取 文件名和后缀
 
 ======== END ========
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<view>
 		<button @click="click">click</button>
+		<button @click="click2">url file</button>
 	</view>
 </template>
-
 <script>
 	export default {
 		data() {
@@ -11,11 +11,22 @@
 		},
 		methods: {
 			click() {
-				
+				uni.downloadFile({
+					url: 'http://192.168.31.148:3000/download/header', //仅为示例，并非真实的资源
+					success: (res) => {
+						console.log(res);
+					}
+				});
+			},
+			click2() {
+				uni.downloadFile({
+					url: 'http://192.168.31.148:3000/file/uni-app.pdf', //仅为示例，并非真实的资源
+					success: (res) => {
+						console.log(res);
+					}
+				});
 			}
 		}
 	}
 </script>
-
-
 <style></style>
