da610a764d378aea577375520e554e961c2cc3fd
愈合 <1641284022@qq.com>
2025-02-27 16:06:50 +0800

    更新示例

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,12 @@
 <template>
 	<view>
-		<button @click="click">click</button>
+		<button @click="click('')">click</button>
 		<button @click="click2">url file</button>
+		<button @click="click3">send file</button>
+		<button @click="click(1)">click header1</button>
+		<button @click="click(2)">click header2</button>
+		<button @click="click(3)">click header3</button>
+		<button @click="click(4)">click header4</button>
 	</view>
 </template>
 <script>
@@ -10,9 +15,11 @@
 			return {}
 		},
 		methods: {
-			click() {
+			click(num) {
+				let str = 'header' + num
+				console.log("str", str)
 				uni.downloadFile({
-					url: 'http://192.168.31.148:3000/download/header', //仅为示例，并非真实的资源
+					url: `http://192.168.31.148:3000/download/${str}`, //仅为示例，并非真实的资源
 					success: (res) => {
 						console.log(res);
 					}
@@ -21,6 +28,14 @@
 			click2() {
 				uni.downloadFile({
 					url: 'http://192.168.31.148:3000/file/uni-app.pdf', //仅为示例，并非真实的资源
+					success: (res) => {
+						console.log(res);
+					}
+				});
+			},
+			click3() {
+				uni.downloadFile({
+					url: 'http://192.168.31.148:3000/download/senfile', //仅为示例，并非真实的资源
 					success: (res) => {
 						console.log(res);
 					}
