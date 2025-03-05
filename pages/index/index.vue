426cf3d46b5b5010d20c80a997dc16087dd76b4d
愈合 <1641284022@qq.com>
2025-03-05 11:40:40 +0800

    更新示例

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,25 @@
 <template>
-  <view>
-    <uni-data-picker v-model="temperature" readonly placeholder="请选择最高气温"
-      :localdata="temperaturePicker"></uni-data-picker>
-  </view>
+	<view>
+		<uni-data-picker v-model="temperature" placeholder="请选择最高气温"
+			:localdata="temperaturePicker"></uni-data-picker>
+	</view>
 </template>
-
 <script>
-  export default {
-    data() {
-      return {
-        temperature: "25",
-        temperaturePicker: []
-      }
-    },
-    onLoad() {
-      for (var i = 50; i > -41; i--) {
-        this.temperaturePicker.push({
-          text: `${i}℃`,
-          value: `${i}`
-        });
-      }
-    }
-  }
+	export default {
+		data() {
+			return {
+				temperature: 25,
+				temperaturePicker: [],
+				show: false
+			}
+		},
+		onLoad() {
+			for (var i = 50; i > -41; i--) {
+				this.temperaturePicker.push({
+					text: `${i}℃`,
+					value: i
+				});
+			}
+		}
+	}
 </script>
