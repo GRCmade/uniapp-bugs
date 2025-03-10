83eab787345ce9702e248af99d20e5361698a253
愈合 <1641284022@qq.com>
2025-03-10 17:45:35 +0800

    h5-getRegion-cli

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,25 +1,38 @@
 <template>
-  <view>
-    <button @click="click">click</button>
-    <uni-badge text="1"></uni-badge>
-    <uni-badge text="2" type="success" @click="bindClick"></uni-badge>
-    <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
-  </view>
+  {{ lon+' '+lat }}
+  <map id="unimap" class="w-750 h-full" :longitude="lon" :latitude="lat" @markertap="markertap"
+    @regionchange="regionchange"></map>
 </template>
 
+<script setup>
+import { onMounted, ref, getCurrentInstance, nextTick } from 'vue';
+const map = ref(null)
+const { ctx } = getCurrentInstance();
+const lon = ref(34)
+const lat = ref(34)
 
-<script>
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    click() {
+// var map;
+onMounted(() => {
+  getre()
+});
 
-    }
-  }
+async function regionchange(event) {
+  if(event.type == 'begin')return
+  console.log('范围变化', event, map.value);
+  lon.value = event.detail.centerLocation.longitude
+  lat.value = event.detail.centerLocation.latitude
+  getre()
+}
+
+async function getre() {
+  uni.createMapContext('unimap', ctx).getRegion({
+    success: function (res) {
+      console.log('视野范围:', res);
+    },
+    fail: function (err) {
+      console.error('获取视野范围失败:', err);
+    },
+    complete: () => { }
+  });
 }
 </script>
-
-
-<style></style>
