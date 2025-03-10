22ef596a7eebb2b7db92b3deb47a2f1eab2bc7ab
愈合 <1641284022@qq.com>
2025-03-10 19:27:29 +0800

    新增自定义组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,7 @@
     <uni-badge text="1"></uni-badge>
     <uni-badge text="2" type="success" @click="bindClick"></uni-badge>
     <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+    <com></com>
   </view>
 </template>
 
