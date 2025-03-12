3d4ef248ec9a5057bda5ae822e01661f03300e24
愈合 <1641284022@qq.com>
2025-03-12 11:23:45 +0800

    ios-uniui-swa-slot

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 ======== README.md ========
 
-# uniapp-bugs 的 template-cli 模板 now-cli
+# uni-swipe-action 在部分ios机型上自定义插槽后点击事件的范围会错位
 > uniapp-bugs https://github.com/GRCmade/uniapp-bugs
 
 - npx @dcloudio/uvm@latest
 - pnpm add @dcloudio/uni-mp-weixin@/Users/gaoruicheng/Documents/DcloudProject/uni-app-next/packages/uni-mp-weixin
 # 问题
 
+点击插槽之后，触发的事件错位，
+经过实际测试，没有复现
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250312112219343.png)
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250312112250189.png)
 ======== END ========
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,26 +1,92 @@
 <template>
-  <view>
-    <button @click="click">click</button>
-    <uni-badge text="1"></uni-badge>
-    <uni-badge text="2" type="success" @click="bindClick"></uni-badge>
-    <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
-    <com></com>
-  </view>
+  <scroll-view scroll-y class="scroll-content">
+    <uni-swipe-action>
+      <uni-swipe-action-item>
+        <uni-card @click="card点击事件" class="uni-conntct-card">
+        </uni-card>
+        <template v-slot:left>
+          <view class="right-slide" @click.stop="重命名点击事件()">
+            <view>
+              <uni-icons custom-prefix="iconfont" type="icon-zhongmingming" size="5vw" />
+              <br />
+              <text>重命名</text>
+            </view>
+          </view>
+          <view class="right-slide" @click.stop="删除点击事件()">
+            <view>
+              <uni-icons type="trash-filled" size="5vw"></uni-icons>
+              <br />
+              <text>删除</text>
+            </view>
+          </view>
+        </template>
+      </uni-swipe-action-item>
+    </uni-swipe-action>
+  </scroll-view>
 </template>
-
 
 <script>
 export default {
   data() {
-    return {}
+    return {
+      // 这里可以添加您需要的数据
+    }
   },
   methods: {
-    click() {
-
+    card点击事件() {
+      console.log('卡片被点击了')
+      uni.showToast({
+        title: '卡片被点击',
+        icon: 'none'
+      })
+    },
+    重命名点击事件() {
+      console.log('重命名按钮被点击了')
+      uni.showToast({
+        title: '重命名操作',
+        icon: 'none'
+      })
+    },
+    删除点击事件() {
+      console.log('删除按钮被点击了')
+      uni.showModal({
+        title: '提示',
+        content: '确定要删除吗？',
+        success: (res) => {
+          if (res.confirm) {
+            uni.showToast({
+              title: '删除成功',
+              icon: 'success'
+            })
+          }
+        }
+      })
     }
   }
 }
 </script>
 
+<style scoped>
+.scroll-content {
+  height: 80vh;
+  /* Adjust height to leave room for the tabBar */
+  overflow-y: scroll;
+}
 
-<style></style>
+.uni-conntct-card {
+  margin-bottom: 0% !important;
+  height: 15vh;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+
+.right-slide {
+  overflow-wrap: anywhere;
+  height: 100%;
+  justify-content: center;
+  width: 18vw;
+  text-align: center;
+  align-items: center;
+  display: flex;
+}
+</style>
