ba0fa18b40c63bd744185f50762135e413bbed65
愈合 <1641284022@qq.com>
2025-03-13 19:59:48 +0800

    更新测试代码

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-    <button @click="click">relaunch click</button>
+    <button @click="click">caniuse click</button>
   </view>
 </template>
 
@@ -12,8 +12,12 @@ export default {
   },
   methods: {
     click() {
-      console.log("uni.canIUse('hideHomeButton')", uni.canIUse('hideHomeButton'))
-      console.log("uni.canIUse('scanCode')", uni.canIUse('scanCode'))
+      // uni.openBluetoothAdapter() 这个没在 todo 中写，就没报错
+			// setScreenBrightness 这个在 todo中写了，就不会报错，会输出不支持
+      console.log("app 支持 h5 不支持 openBluetoothAdapter", uni.canIUse('openBluetoothAdapter'))
+      console.log("app 不支持 h5 支持 chooseFile", uni.canIUse('chooseFile'))
+      console.log("app 支持 h5 支持 getNetworkType", uni.canIUse('getNetworkType'))
+      console.log("app 不支持 h5 不支持 createMediaContainer", uni.canIUse('createMediaContainer'))
       // uni.reLaunch({
       //   url: '/pages/index/relaunch'
       // })
