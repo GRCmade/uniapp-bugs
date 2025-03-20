6941ddb9261cb385fa7ccf16235c56f4c213b9a8
yuhe <1641284022@qq.com>
2025-03-20 20:09:34 +0800

    android-getimageinfo-orientation

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 ======== README.md ========
 
-# uniapp-bugs 的 template-cli 模板 now-cli
+# uni.getImageInfo不能正确获取图片方向
 > uniapp-bugs https://github.com/GRCmade/uniapp-bugs
 
 链接 next 仓库
 
 # 问题
 
+经过实际测试，问题确实存在.
 
+在选择图片的时候，
+
+这样旋转
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/picgo/20250320200735.png)
+
+或者这样旋转
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/picgo/20250320200758.png)
+
+获取到的图片都是 up
 ======== END ========
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,38 @@
 <template>
-  <view>
-    <button @click="click">click</button>
-    <uni-badge text="1"></uni-badge>
-    <uni-badge text="2" type="success" @click="bindClick"></uni-badge>
-    <uni-badge text="3" type="primary" :inverted="true"></uni-badge>
-    <com></com>
-  </view>
+	<view>
+		<button @click="click">click</button>
+		<uni-badge text="1"></uni-badge>
+		<uni-badge text="2" type="success" @click="bindClick"></uni-badge>
+		<uni-badge text="3" type="primary" :inverted="true"></uni-badge>
+		<com></com>
+	</view>
 </template>
 
 
 <script>
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    click() {
-      // uni.navigateTo({
-      //   url: '/pages/index/index-next'
-      // })
-    }
-  }
-}
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			click() {
+				uni.chooseImage({
+					count: 1,
+					sizeType: ['original'],
+					sourceType: ['album', 'camera'],
+					success: (res) => {
+						console.log(res)
+						uni.getImageInfo({
+							src: res.tempFilePaths[0],
+							success: (res) => {
+								console.log("图片信息", res)
+							}
+						})
+					}
+				})
+			}
+		}
+	}
 </script>
 
 
