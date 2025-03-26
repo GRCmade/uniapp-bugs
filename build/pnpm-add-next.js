df253ce7c310a0bf17425a1abd350ca56346af4f
yuhe <1641284022@qq.com>
2025-03-26 19:26:26 +0800

    script

--- build/pnpm-add-next.js
+++ build/pnpm-add-next.js
@@ -14,7 +14,7 @@ const rootDir = path.resolve(__dirname, '..');
 // package.json 文件路径
 const packageJsonPath = path.join(rootDir, 'package.json');
 // uni-app-next 本地路径
-const uniAppNextPath = '/Volumes/valum/Dcloud/uni-app-next';
+const uniAppNextPath = '/Volumes/valum/Dcloud/uni-app-next/packages';
 
 // 读取 package.json 文件
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
