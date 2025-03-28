c60d872c9f29c32eba86a797e59ab870f83f723c
yuhe <1641284022@qq.com>
2025-03-28 11:17:18 +0800

    h5-textarea-blur

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ======== README.md ========
 
-# uniapp-bugs 的 template-cli 模板 now-cli
+# textarea 快速输入的情况下，会导致焦点消失
 > uniapp-bugs https://github.com/GRCmade/uniapp-bugs
 
 链接 next 仓库
 
 # 问题
 
+非常简单，有一个textarea，快速输入的情况下，会导致焦点消失
 
 ======== END ========
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,29 +1,49 @@
 <template>
-  <view>
-    <button @click="click">click</button>
-    <uni-badge text="1"></uni-badge>
-    <com></com>
-
-  </view>
+	<view class="content">
+		<textarea v-model="value"></textarea>
+	</view>
 </template>
 
+<script>
+	export default {
+		data() {
+			return {
+				value: ''
+			}
+		},
+		onLoad() {
 
-<script lang="ts">
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    click() {
-      // uni.navigateTo({
-      //   url: '/pages/index/index-next'
-      // })
-      const res: string = '1'
-      console.log(res)
-    }
-  }
-}
+		},
+		methods: {
+
+		}
+	}
 </script>
 
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 
-<style></style>
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
