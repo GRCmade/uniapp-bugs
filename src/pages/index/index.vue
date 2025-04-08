45b53d6c812c1e147b777cd6e5ce4289ec7a0a48
yuhe <1641284022@qq.com>
2025-04-08 16:17:50 +0800

    ios-movable-view-change

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,65 @@
 <template>
-  <view>
-    <!-- <uni-badge text="1"></uni-badge> -->
-    <!-- <com></com> -->
-    <button @click="click">click 123 </button>
-
+  <view style="margin-top: 50px;" class="test-page">
+    <movable-area>
+      <movable-view direction="vertical" @touchstart="handleDragStart" @longpress="handleLongpress"
+        @touchmove="handleTouchMove" @change="handleMoving" @touchend="handleDragEnd">text</movable-view>
+    </movable-area>
   </view>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-const click = () => {
+<script>
+export default {
+  components: {},
+  data() {
+    return {}
+  },
+  onLoad() {
 
+  },
+  onPageScroll(event) {
+    console.log('0-0-0-0-0-0-0-0-0-0-0-0-0-0-');
+  },
+  methods: {
+    handleDragStart(event) {
+      console.log('---1111---开始拖动', event);
+    },
+    handleLongpress(event) {
+      console.log('---2222---长按事件', event);
+    },
+    handleTouchMove(event) {
+      console.log('---3333---TouchMove事件', event);
+    },
+    handleMoving(event) {
+      console.log('---4444---change事件', event);
+    },
+    handleDragEnd(event) {
+      console.log('---5555---拖动结束', event);
+    }
+  }
 }
 </script>
 
+<style lang="scss" scoped>
+.test-page {
+  height: 100vh;
+  overflow: auto;
 
-<style></style>
+  movable-view {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 150rpx;
+    width: 150rpx;
+    background-color: #007AFF;
+    color: #fff;
+  }
+
+  movable-area {
+    height: 100vh;
+    width: 100%;
+    background-color: #D8D8D8;
+    overflow: hidden;
+  }
+
+}
+</style>
