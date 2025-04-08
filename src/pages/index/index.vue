ac10315f247917465bf4fcccc991df32f8a4fb27
yuhe <1641284022@qq.com>
2025-04-08 14:47:15 +0800

    change index

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,16 +1,16 @@
 <template>
   <view>
-    <button @click="click">click 123 </button>
     <!-- <uni-badge text="1"></uni-badge> -->
-    <com></com>
+    <!-- <com></com> -->
+    <button @click="click">click 123 </button>
+
   </view>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-const str = ref<string>('123')
 const click = () => {
-  console.log('click', str.value)
+
 }
 </script>
 
