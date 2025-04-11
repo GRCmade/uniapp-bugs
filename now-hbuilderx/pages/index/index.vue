0b5903f11f669d909cec581525fd54f0af47a1cf
yuhe <1641284022@qq.com>
2025-04-11 16:40:14 +0800

    xhs-button-open-type

--- now-hbuilderx/pages/index/index.vue
+++ now-hbuilderx/pages/index/index.vue
@@ -1,18 +1,30 @@
 <template>
   <view>
-    <button @click="click">click 123 </button>
-    <!-- <uni-badge text="1"></uni-badge> -->
-    <com></com>
+    <button open-type="share">share</button>
+    <button open-type="openSetting">openSetting</button>
+    <button open-type="contact">contact</button>
+    <button open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">getPhoneNumber</button>
+    <button type="button" open-type="getUserInfo" lang="zh_CN" @getuserinfo="getUser">获取用户信息</button>  
   </view>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-const str = ref<string>('123')
-const click = () => {
-  console.log('click', str.value)
+<script lang="ts">
+export default {
+  methods: {
+    getUser(e: any) {
+      console.log('用户信息', e)
+    },
+    getPhoneNumber(e: any) {
+      console.log('获取手机号', e)
+    }
+  },
+  onShareAppMessage() {
+    return {
+      title: '分享',
+      path: '/pages/index/index'
+    }
+  }
 }
 </script>
-
 
 <style></style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,18 +1,30 @@
 <template>
   <view>
-    <!-- <uni-badge text="1"></uni-badge> -->
-    <!-- <com></com> -->
-    <button @click="click">click 123 </button>
-
+    <button open-type="share">share</button>
+    <button open-type="openSetting">openSetting</button>
+    <button open-type="contact">contact</button>
+    <button open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">getPhoneNumber</button>
+    <button type="button" open-type="getUserInfo" lang="zh_CN" @getuserinfo="getUser">获取用户信息</button>  
   </view>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-const click = () => {
-
+<script lang="ts">
+export default {
+  methods: {
+    getUser(e: any) {
+      console.log('用户信息', e)
+    },
+    getPhoneNumber(e: any) {
+      console.log('获取手机号', e)
+    }
+  },
+  onShareAppMessage() {
+    return {
+      title: '分享',
+      path: '/pages/index/index'
+    }
+  }
 }
 </script>
-
 
 <style></style>
