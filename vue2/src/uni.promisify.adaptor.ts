204bcb29f6d3a76addd120fcc3fb9e827f9fd944
愈合 <1641284022@qq.com>
2025-04-24 20:30:12 +0800

    更新项目结构,改为vue2和vue3一体

--- vue2/src/uni.promisify.adaptor.ts
+++ vue2/src/uni.promisify.adaptor.ts
@@ -1,13 +1,10 @@
 uni.addInterceptor({
-  returnValue(res: any): any {
+  returnValue (res) {
     if (!(!!res && (typeof res === "object" || typeof res === "function") && typeof res.then === "function")) {
       return res;
     }
     return new Promise((resolve, reject) => {
-      res.then((res: any) => {
-        if (!res) return resolve(res);
-        return res[0] ? reject(res[0]) : resolve(res[1]);
-      });
+      res.then((res: [unknown, unknown]) => res[0] ? reject(res[0]) : resolve(res[1]));
     });
   },
-}); 
+});
