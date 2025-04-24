6a3a91143a182c9576652135e0824bbb88fc91ce
愈合 <1641284022@qq.com>
2025-04-24 19:40:43 +0800

    h5-choose-fail

--- now-hbuilderx/pages/index/index.vue
+++ now-hbuilderx/pages/index/index.vue
@@ -1,16 +1,41 @@
 <template>
   <view>
-    <button @click="click">click 123 </button>
     <!-- <uni-badge text="1"></uni-badge> -->
-    <com></com>
+    <!-- <com></com> -->
+    <button @click="chooseImage">chooseImage</button>
+    <button @click="chooseFile">chooseFile</button>
+    <button @click="chooseVideo">chooseVideo</button>
+
   </view>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-const str = ref<string>('123')
-const click = () => {
-  console.log('click', str.value)
+const chooseImage = () => {
+  uni.chooseImage({
+    sizeType: ['original', 'compressed'],
+    sourceType: ['album', 'camera'],
+    success: ({ tempFilePaths, tempFiles }) => { },
+    fail: (error) => {
+      console.log("error", error)
+    }
+  })
+}
+const chooseFile = () => {
+  uni.chooseFile({
+    success: ({ tempFilePaths, tempFiles }) => { },
+    fail: (error) => {
+      console.log("error", error)
+    }
+  })
+}
+const chooseVideo = () => {
+  uni.chooseVideo({
+    success: () => { },
+    fail: (error) => {
+      console.log("error", error)
+    }
+  })
 }
 </script>
 
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,15 +2,40 @@
   <view>
     <!-- <uni-badge text="1"></uni-badge> -->
     <!-- <com></com> -->
-    <button @click="click">click 123 </button>
+    <button @click="chooseImage">chooseImage</button>
+    <button @click="chooseFile">chooseFile</button>
+    <button @click="chooseVideo">chooseVideo</button>
 
   </view>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-const click = () => {
-  
+const chooseImage = () => {
+  uni.chooseImage({
+    sizeType: ['original', 'compressed'],
+    sourceType: ['album', 'camera'],
+    success: ({ tempFilePaths, tempFiles }) => { },
+    fail: (error) => {
+      console.log("error", error)
+    }
+  })
+}
+const chooseFile = () => {
+  uni.chooseFile({
+    success: ({ tempFilePaths, tempFiles }) => { },
+    fail: (error) => {
+      console.log("error", error)
+    }
+  })
+}
+const chooseVideo = () => {
+  uni.chooseVideo({
+    success: () => { },
+    fail: (error) => {
+      console.log("error", error)
+    }
+  })
 }
 </script>
 
