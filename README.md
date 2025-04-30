42a1a8e05cddf5e709f3c84a467e127f5e38eef5
愈合 <1641284022@qq.com>
2025-04-30 11:14:14 +0800

    harmony-webview-clickevent

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ======== README.md ========
 
-# 标题
+# 鸿蒙 webview 添加margin之后点击事件触发位置不正确
+vue3
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250430111210141.png)
 
+主要的内容就是加入到了 margin 就会生效
+
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250430111250752.png)
 ======== END ========
--- vue3/src/pages/index/index.vue
+++ vue3/src/pages/index/index.vue
@@ -1,18 +1,38 @@
 <template>
-  <view>
-    <!-- <uni-badge text="1"></uni-badge> -->
-    <!-- <com></com> -->
-    <button @click="click">click 123 </button>
-
-  </view>
+	<view>
+		<!-- #ifndef APP-HARMONY -->
+		<uni-nav-bar :title="title" backgroundColor="#f1f1f1" left-icon="left" @clickLeft="hmBackPress" fixed="true"
+			statusBar="true" :border="false"></uni-nav-bar>
+		<!-- #endif -->
+		<web-view :src="url" style="margin-top: 500rpx;"></web-view>
+	</view>
 </template>
-
-<script lang="ts" setup>
-import { ref } from 'vue'
-const click = () => {
-  
-}
+<script>
+	export default {
+		data() {
+			return {
+				title: '在线客服',
+				jjdm: '',
+				url: '',
+			}
+		},
+		onLoad() {
+			this.url = 'http://192.168.31.227:5500/vue3/src/static/index.html'
+			// this.url = 'https://care60.live800.com/live800/chatClient/chatbox.jsp?companyID=80028932'
+		},
+		methods: {
+			hmBackPress() {}
+		}
+	}
 </script>
-
-
-<style></style>
+<style>
+	.box {
+		/* position: fixed; */
+		position: relative;
+		left: 0;
+		right: 0;
+		background-color: aqua;
+		width: 100px;
+		height: 100px;
+	}
+</style>
