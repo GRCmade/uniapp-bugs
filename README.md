5e877b65a206381acbe8567527859218a32b9d8c
愈合 <1641284022@qq.com>
2025-05-08 15:15:46 +0800

    harmony-image-tempfilepath-lifecircle

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ======== README.md ========
 
-# 标题
+# 鸿蒙 image 组件加载临时路径在页面刚加载的时候存在问题
+
+在页面刚加载的时候 onready , data 中赋值,就不会生效
+
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250508151306593.png)
+
+但是经过 button 点击之后 , 就会生效 , 或者说延迟一会赋值 , 就正常了
+
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250508151349111.png)
+
+经过反馈 45 正常 45 之后失效了
 
 ======== END ========
--- vue3/src/pages/index/index.vue
+++ vue3/src/pages/index/index.vue
@@ -1,17 +1,52 @@
 <template>
-  <view>
-    <!-- <uni-badge text="1"></uni-badge> -->
-    <!-- <com></com> -->
-    <button @click="click">click 123</button>
+  <view class="content">
+    <image class="logo" src="/static/logo.png"></image>
+    <view class="text-area">
+      <button @tap="saveImage">保存图片到本地</button>
+    </view>
+    <view>
+      <view class="padding">imagePath : {{ imagePath }}</view>
+      <image :src="imagePath"></image>
+      <view class="padding">https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png</view>
+      <image :src="'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png'"></image>
+      <view class="padding">/data/storage/el2/base/cache/HBuilder/uni-download/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf(3).png</view>
+
+      <image :src="'/data/storage/el2/base/cache/HBuilder/uni-download/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf(3).png'"></image>
+    </view>
   </view>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-const num = ref(0)
-const click = () => {
-  console.log(num)
+<script>
+export default {
+  onLoad() {
+    this.imagePath = uni.getStorageSync('imagePath')
+    // uni.showModal({
+    //   content: JSON.stringify(this.imagePath)
+    // })
+  },
+  data() {
+    return {
+      title: 'Hello',
+      imagePath: '/data/storage/el2/base/cache/HBuilder/uni-download/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf(3).png'
+    }
+  },
+  methods: {
+    saveImage() {
+      uni.downloadFile({
+        url: 'https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png',
+        success: res => {
+          this.imagePath = res.tempFilePath
+          console.log('res.tempFilePath', res.tempFilePath)
+          uni.setStorageSync('imagePath', res.tempFilePath)
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style></style>
+<style>
+.padding {
+  padding: 20rpx;
+}
+</style>
