6edecafa83c59676d85403fea72dbb3ca65187bf
愈合 <1641284022@qq.com>
2025-05-14 19:25:02 +0800

    mp-weixin-export-default

--- vue3/src/pages/index/index.vue
+++ vue3/src/pages/index/index.vue
@@ -1,17 +1,39 @@
 <template>
-  <view>
-    <!-- <uni-badge text="1"></uni-badge> -->
-    <!-- <com></com> -->
-    <button @click="click">click 123</button>
-  </view>
+  <view class="container">演示Vue3版微信小程序import错误</view>
 </template>
-
-<script lang="ts" setup>
-import { ref } from 'vue'
-const num = ref(0)
-const click = () => {
-  console.log(num)
+<script>
+// #ifdef VUE3
+/**
+ * VUE3编译：app端不再支持require
+ * 微信小程序调试时全部支持，发行版不支持export defaut。（不知道什么不统一！！！，可能通过微信开发者查看源码是uniapp官方的问题)
+ */
+import { filepickerESM } from '../../static/js/wxy-file-picker-esm.js'
+console.log('filepickerESM =>', filepickerESM)
+//微信小程序调试时支持，但发行版时是undefined。查看微信开发者中源码是uniapp编译的问题
+import filepickerMixin from '../../static/js/wxy-file-picker-mixin.js'
+console.log('filepickerMixin =>', filepickerMixin)
+// #endif
+export default {
+  // #ifdef VUE3
+  // mixins: [filepickerESM],
+  mixins: [filepickerMixin], //微信小程序编译发行版，因为    filepickerMixin是undefined导致失败报错
+  // #endif
+  data() {
+    return {}
+  },
+  onReady() {
+    this.test1()
+  },
+  methods: {
+    test1() {
+      // console.log('test =>', this.test('Hello World'))
+    }
+  }
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.container {
+  padding: 10px 20px;
+}
+</style>
