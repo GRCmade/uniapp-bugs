4c935b228b205b9d920fe51f2eb8fdf9abda8ede
愈合 <1641284022@qq.com>
2025-06-04 16:45:18 +0800

    mp-weixin-coverview-view

--- vue3/src/pages/index/index.vue
+++ vue3/src/pages/index/index.vue
@@ -1,8 +1,8 @@
 <template>
   <view>
     <video id="myVideo" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4" @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls>
-      <cover-view class="video-mask">cover-view</cover-view>
-      <view class="video-mask">view</view>
+      <cover-view class="video-mask" @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchcancel="handleTouchCancel" @touchend="handleTouchEnd">cover-view</cover-view>
+      <view class="video-mask" @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchcancel="handleTouchCancel" @touchend="handleTouchEnd">view</view>
     </video>
   </view>
 </template>
@@ -13,12 +13,25 @@ const danmuList = ref([])
 const videoErrorCallback = (e: any) => {
   console.log(e)
 }
+const handleTouchStart = (e: any) => {
+  console.log(e)
+}
+const handleTouchMove = (e: any) => {
+  console.log(e)
+}
+const handleTouchEnd = (e: any) => {
+  console.log(e)
+}
+const handleTouchCancel = (e: any) => {
+  console.log(e)
+}
 </script>
 
 <style>
 .video-mask {
-  width: 100px;
-  height: 100px;
+  width: 100%;
+  height: 50px;
+  margin-top: 50px;
   background-color: #0075ff;
   display: flex;
   justify-content: center;
