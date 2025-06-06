08711213268bcf246acec40ecd9238751a1b5254
愈合 <1641284022@qq.com>
2025-06-06 19:43:33 +0800

    add uniapp-hbuilderx

---
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<button @click="handleClick">Click me</button>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			message: 'Hello, World!'
+		}
+	},
+	methods: {
+		handleClick() {
+			console.log('Button clicked')
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style>
