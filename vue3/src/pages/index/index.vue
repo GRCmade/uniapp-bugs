ade9602c7abdbcdc8d6c22e7e528f62dcc448ea1
愈合 <1641284022@qq.com>
2025-06-09 16:38:26 +0800

    harmony-previewImage-modulejson5

--- vue3/src/pages/index/index.vue
+++ vue3/src/pages/index/index.vue
@@ -1,15 +1,17 @@
 <template>
-  <view>
-    <button @click="click">button</button>
-  </view>
+	<view>
+		<button @click="click">button</button>
+	</view>
 </template>
-
 <script lang="ts" setup>
-const click = () => {
-  // uni.navigateTo({
-  //   url: '/pages/index/index-next'
-  // })
-}
+	const click = () => {
+		uni.previewImage({
+			urls: ['https://cos.haoce.com/upload/image/20250606/54a19f76-8b3b-4e67-ae1a-c6aec2c16e70.jpg'],
+			current: "0",
+			fail(e:any) {
+				console.log("e", e)
+			}
+		})
+	}
 </script>
-
 <style></style>
