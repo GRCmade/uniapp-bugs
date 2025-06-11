9288bfdf9fd63eae30acdc83fd11295c8261c050
愈合 <1641284022@qq.com>
2025-06-11 14:41:08 +0800

    harmony-textarea-autoheight-page

--- vue3/src/pages/index/index.vue
+++ vue3/src/pages/index/index.vue
@@ -1,15 +1,25 @@
 <template>
   <view>
-    <button @click="click">button</button>
+    <view>{{ text }}</view>
+    <textarea class="textarea" v-model="text" auto-height />
   </view>
 </template>
 
 <script lang="ts" setup>
-const click = () => {
-  // uni.navigateTo({
-  //   url: '/pages/index/index-next'
-  // })
-}
+import { ref } from 'vue'
+const text = ref('')
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+page {
+  background-color: aqua !important;
+}
+.textarea {
+  width: 100%;
+  height: 100px;
+  border: 1px solid red;
+  border-radius: 10px;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
