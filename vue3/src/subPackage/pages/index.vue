27650a48d1b8e3e1e0c6d0ac25fbba88e44d2018
愈合 <1641284022@qq.com>
2025-06-11 19:13:20 +0800

    mp-weixin-subpackages-js

--- vue3/src/subPackage/pages/index.vue
+++ vue3/src/subPackage/pages/index.vue
@@ -7,6 +7,10 @@
 
 <script lang="ts" setup>
 import { test } from '../utils/test'
+import areas from '@/subPackage/static/areas.json'
+
+console.log(areas)
+
 const click = () => {
   test()
 }
---
+++ vue3/src/subPackage/visit/visit.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>
+    <text>visit</text>
+  </view>
+</template>
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+import areas from '@/subPackage/static/areas.json'
+
+onMounted(() => {
+  console.log(areas)
+})
+</script>
+<style lang="scss" scoped></style>
