b418d21f2609ea138002b7ae33a183d8468dc9e2
愈合 <1641284022@qq.com>
2025-06-16 17:15:38 +0800

    harmony-showloading-cehua

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ======== README.md ========
 
-# 标题
+# 鸿蒙中 showLoading 无法侧滑关闭
+
+新增了这段代码:
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250616171458838.png)
 
 ======== END ========
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,24 @@
 <template>
   <view>
     <button @click="click">button</button>
+    <button @click="hideLoading">hideLoading</button>
   </view>
 </template>
 
 <script lang="ts" setup>
-const click = () => {}
+const click = () => {
+  uni.showLoading({
+    title: '加载中',
+    mask: true
+  })
+  setTimeout(() => {
+    uni.hideLoading()
+  }, 4000)
+}
+
+const hideLoading = () => {
+  uni.hideLoading()
+}
 </script>
 
 <style></style>
