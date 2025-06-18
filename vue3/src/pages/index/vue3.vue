e76aa4bcf63a5b469776d888b2aba582fe9e8c99
愈合 <1641284022@qq.com>
2025-06-18 14:58:58 +0800

    harmony-choosemedia-file-protocol

--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,46 @@
 <template>
   <view>
-    <button @click="click">button</button>
+    <button @click="chooseImage">chooseImage</button>
+    <button @click="chooseMedia">chooseMedia</button>
   </view>
 </template>
 
 <script lang="ts" setup>
-const click = () => {}
+const chooseImage = () => {
+  uni.chooseImage({
+    count: 1,
+    sourceType: ['album'],
+    success: res => {
+      try {
+        getSyncFile(res.tempFilePaths[0])
+      } catch (err) {
+        console.log('readFileSync', err) //
+      }
+    },
+    fail: err => {
+      console.log('chooseMedia', err)
+    }
+  })
+}
+
+const chooseMedia = () => {
+  uni.chooseMedia({
+    count: 1,
+    success: res => {
+      getSyncFile(res.tempFiles[0].tempFilePath)
+    },
+    fail: err => {
+      console.log('chooseMedia', err)
+    }
+  })
+}
+
+const getSyncFile = (path: string) => {
+  console.log('getSyncFile', path)
+  const fileSystemManager = uni.getFileSystemManager()
+  const res = fileSystemManager.readFileSync(path)
+  console.log(res)
+}
 </script>
 
 <style></style>
