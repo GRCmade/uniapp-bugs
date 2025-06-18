890e1525c30db99d8881f098e3a4aab78cc795d6
愈合 <1641284022@qq.com>
2025-06-18 19:31:56 +0800

    Revert "publish"
    This reverts commit b7a5156330c8f65dc61fe1612315f2321f46887b.

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -1,26 +1,23 @@
 <template>
 	<view>
-		<button @click="show">显示原生状态栏</button>
-		<button @click="hide">隐藏原生状态栏</button>
+		<button @click="handleClick">Click me</button>
 	</view>
 </template>
+
 <script>
-	import { showStatusBar, hideStatusBar } from '@/uni_modules/harmony-statusbar';
-	export default {
-		data() {
-			return {
-				message: 'Hello, World!'
-			}
-		},
-		methods: {
-			show() {
-				showStatusBar()
-			},
-			hide() {
-				hideStatusBar()
-			}
+export default {
+	data() {
+		return {
+			message: 'Hello, World!'
+		}
+	},
+	methods: {
+		handleClick() {
+			console.log('Button clicked')
 		}
 	}
+}
 </script>
+
 <style scoped>
 </style>
