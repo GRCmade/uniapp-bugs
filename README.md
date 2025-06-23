aa8c91fa558ddc730769cdefdb392046dbd6cf09
愈合 <1641284022@qq.com>
2025-06-23 14:41:09 +0800

    app-swiper-vif-bug

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ======== README.md ========
 
-# 标题
+# app 上切换swiper显示异常的问题
+
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250623144018220.png)
 
 ======== END ========
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,47 @@
 <template>
   <view>
-    <button @click="click">button</button>
+    <button @tap="change">交换</button>
+    <view v-if="flag" class="uni-margin-wrap">
+      <swiper class="swiper">
+        <swiper-item>
+          <view class="swiper-item uni-bg-red">A</view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item uni-bg-green">B</view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item uni-bg-blue">C</view>
+        </swiper-item>
+      </swiper>
+    </view>
+
+    <view v-if="!flag" class="uni-margin-wrap">
+      <swiper class="swiper">
+        <swiper-item>
+          <view class="swiper-item uni-bg-red">D</view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item uni-bg-green">E</view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item uni-bg-blue">F</view>
+        </swiper-item>
+      </swiper>
+    </view>
   </view>
 </template>
 
-<script lang="ts" setup>
-const click = () => {}
+<script>
+export default {
+  data() {
+    return {
+      flag: true
+    }
+  },
+  methods: {
+    change() {
+      this.flag = !this.flag
+    }
+  }
+}
 </script>
-
-<style></style>
