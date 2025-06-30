2c49cec980560d47bd729a154ee339a9d5562134
愈合 <1641284022@qq.com>
2025-06-30 18:57:14 +0800

    app-build-wgt-css-repeat

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -1,23 +1,53 @@
 <template>
-	<view>
-		<button @click="handleClick">Click me</button>
+	<view class="content">
+		<uni-nav-bar title="这是页面1111" />
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area" @click="nav">
+			<text class="title">{{title}}</text>
+		</view>
 	</view>
 </template>
-
 <script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {},
+		methods: {
+			nav() {
+				uni.navigateTo({
+					url: "/pages/index2/index2"
+				})
+			}
 		}
 	}
-}
 </script>
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 
-<style scoped>
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
 </style>
---
+++ uniapp-hbuilderx/pages/index2/index2.vue
@@ -0,0 +1,17 @@
+<!--
+/**
+ * 作者: TanXiang
+ * 日期: TODO DATE
+ * 描述: TODO 编写文件内容描述
+ */
+-->
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <uni-nav-bar title="这是页面2222" />
+</template>
+
+<style scoped lang="scss"></style>
