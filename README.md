20c71a9eca61b30026558f77b1971a15ec97623f
愈合 <1641284022@qq.com>
2025-06-30 14:48:08 +0800

    vue2-app-appinterceptor-tabbar

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ======== README.md ========
 
-# 标题
+# vue2 上的 addintercepter 无法拦截 tababr 的切换
+
+之前改过 vue3 的,这个是 vue2 的改动
+
+位于 uniapp-hbuilderx
 
 ======== END ========
--- uniapp-hbuilderx/App.vue
+++ uniapp-hbuilderx/App.vue
@@ -2,12 +2,29 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			this.interceptSwitchTab()
 		},
 		onShow: function() {
 			console.log('App Show')
 		},
 		onHide: function() {
 			console.log('App Hide')
+		},
+		methods: {
+			interceptSwitchTab() {
+				const tabPages = ['/pages/message/index'];
+				uni.addInterceptor('switchTab', {
+					invoke: (args) => {
+						if (tabPages.includes(args.url)) {
+							uni.navigateTo({
+								url: '/pages/login/login'
+							})
+							return false
+						}
+						return true
+					}
+				});
+			}
 		}
 	}
 </script>
