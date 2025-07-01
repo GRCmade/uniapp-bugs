8893a69fe70ba989f6cebfe010a90d515dcca190
愈合 <1641284022@qq.com>
2025-07-01 16:03:47 +0800

    app-movablearea-style-change-proxy

--- vue3/src/components/com/com.vue
+++ vue3/src/components/com/com.vue
@@ -1,7 +1,49 @@
 <template>
-  <div>com</div>
+	<view>
+		<movable-area :style="{ width: '100%', height: allQuestionHeight + 'px' }">
+			<movable-view class="base-drag-wrapper" v-for="item in questions" :key="item.id" :y="item.y" style="width: 100%"
+				direction="vertical" @change="handleMove">
+				<view class="question-item">
+					<text>{{ item.text }}</text>
+				</view>
+			</movable-view>
+		</movable-area>
+	</view>
 </template>
-
-<script setup></script>
-
-<style scoped></style>
+<script>
+	export default {
+		data() {
+			return {
+				questions: [{
+					text: 'com组件',
+					id: 1,
+					y: 0
+				}, {
+					text: ' 123',
+					id: 2,
+					y: 50
+				}],
+				allQuestionHeight: 100
+			}
+		},
+		mounted() {
+			// 注释修改高度的代码就正常显示
+			setTimeout(() => {
+				this.allQuestionHeight = 101
+				console.log('change allQuestionHeight')
+			}, 500)
+		},
+		methods: {
+			handleMove(event) {
+				// 处理移动事件
+				console.log('move event:', event)
+			}
+		}
+	}
+</script>
+<style>
+	.question-item {
+		width: 100%;
+		height: min-content;
+	}
+</style>
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,103 @@
 <template>
-  <view>
-    <button @click="click">button</button>
-  </view>
+	<view class="content">
+		<!-- 		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{ title }}</text>
+		</view> -->
+		<view class="list-main">
+			<com v-if="currentData.type == 1" style="width: 100%"></com>
+			<!-- <text v-if="currentData.type == 1"> {{ currentData.text }} </text> -->
+			<text v-else-if="currentData.type == 2" style="color: #000;">
+				{{ currentData.text }}
+			</text>
+			<view class=""> text:{{currentData.type}} index:{{index}}
+			</view>
+		</view>
+		<view class="next-btn" @click="next">
+			<text>下一题</text>
+		</view>
+		<view class="next-btn" @click="first">
+			<text>第一题</text>
+		</view>
+	</view>
 </template>
-
-<script lang="ts" setup>
-const click = () => {}
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				datas: [{
+					type: 1,
+					text: '组件1'
+				}, {
+					type: 2,
+					text: '组件2'
+				}],
+				index: 0
+			};
+		},
+		computed: {
+			currentData() {
+				return this.datas[this.index];
+			}
+		},
+		onLoad() {},
+		methods: {
+			next() {
+				if (this.index < this.datas.length - 1) {
+					this.index++;
+				}
+			},
+			first() {
+				this.index = 0
+			}
+		}
+	};
 </script>
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
 
-<style></style>
+	.list-main {
+		/* flex: 1; */
+		width: 100%;
+		height: 500px;
+		position: relative;
+		background-color: red;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+
+	.next-btn {
+		width: 100%;
+		height: 50px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: black;
+		color: #fff;
+		margin-bottom: 30px;
+	}
+</style>
