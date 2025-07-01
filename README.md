10f5c2772724542b54de20f89d1838f53b8bbc28
愈合 <1641284022@qq.com>
2025-07-01 18:51:07 +0800

    android-createInnerAudioContext+bug

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ======== README.md ========
 
-# 标题
+# createInnerAudioContext 在 安卓上有特殊符号无法播放
+
+目前测试 有 %2B + 号无法播放
 
 ======== END ========
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,27 @@
 <template>
   <view>
+    {{ audioSrc }}
     <button @click="click">button</button>
+    <button @click="stop">stop</button>
   </view>
 </template>
 
 <script lang="ts" setup>
-const click = () => {}
+import { ref } from 'vue'
+
+const audioContext = ref<any>(null)
+const audioSrc = ref(
+  'https://aizuowen-test.oss-cn-shenzhen.aliyuncs.com/audio/202507/20250701150516686388ac6227a.mp3?Expires=1751612716&OSSAccessKeyId=LTAI5tFRY33oR23Tatejksdx&Signature=nGvxvuAyheTXoazsdla%2Bh4BPQ4Y%3D'
+)
+
+const click = () => {
+  audioContext.value = uni.createInnerAudioContext() // 创建音频上下文
+  audioContext.value.src = audioSrc.value
+  audioContext.value.play() // 开始播放
+}
+const stop = () => {
+  audioContext.value.stop() // 停止播放
+}
 </script>
 
 <style></style>
