4e41f5b35ffc05837ff46e4424f202f981abb609
愈合 <1641284022@qq.com>
2025-07-03 20:18:11 +0800

    harmony-renderjs-html2canvas

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 ======== README.md ========
 
-# 标题
+# 鸿蒙 renderjs 的 html2canvas 跨域问题
 
+在 html2canvas 调用的时候,会触发createElement,创建一个 iframe
+创建iframe之后,调用 Document.open的时候,会触发跨域问题
+
+需要理解一下跨域,跨域是
+
+只要 iframe.src 的 URL 与当前页面的 protocol、host、port 任一不一致，就是“非同源”，浏览器就会阻止你访问 iframe.contentDocument 或调用其方法（如 .open()、.write()）
+
+在这里调用的时候
 ======== END ========
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,79 @@
 <template>
-  <view>
-    <button @click="click">button</button>
+  <view class="abc">
+    <view id="draw" style="width: 100%; height: 300rpx; background-color: greenyellow">
+      我是要截图的内容
+    </view>
+    <view
+      class="btn"
+      @click="canvasToImage.preservation"
+      style="
+        width: 100%;
+        height: 80rpx;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1rpx solid #000;
+        margin-top: 30rpx;
+      ">
+      点我截图
+    </view>
   </view>
 </template>
-
-<script lang="ts" setup>
-const click = () => {}
+<script>
+export default {
+  methods: {
+    showToast(type) {
+      if (type == 'load') {
+        uni.showLoading()
+      } else {
+        uni.hideLoading()
+      }
+    },
+    saveToAlbum(url) {
+      console.log('saveToAlbum', url)
+      this.showToast()
+    }
+  }
+}
 </script>
-
-<style></style>
+<!-- #ifdef APP -->
+<script module="canvasToImage" lang="renderjs">
+import html2canvas from "html2canvas"
+export default {
+	data() {
+		return {}
+	},
+	methods: {
+		async preservation(event, ownerInstance) {
+			console.log("preservation")
+			ownerInstance.callMethod('showToast', 'load')
+			var dom = document.getElementById('draw')
+			try {
+				console.log("dom",dom)
+				const canvas = await html2canvas(dom, {
+					background: '#fff',
+					scale: 2,
+					useCORS: true,
+					allowTaint: true,
+					logging: true
+				})
+				console.log("canvas", canvas)
+				let url = canvas.toDataURL('image/png')
+				ownerInstance.callMethod('saveToAlbum', url)
+			} catch (error) {
+				console.log(JSON.stringify(error))
+				ownerInstance.callMethod('showToast', 'hide')
+				//TODO handle the exception
+			}
+		},
+	}
+}
+</script>
+<!-- #endif -->
+<style>
+.controls {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20rpx;
+}
+</style>
