510ca8cf944e064fe0fc7c153d79fc579334c920
愈合 <1641284022@qq.com>
2025-07-08 16:30:42 +0800

    harmony-request-query-repeat

--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -6,14 +6,27 @@
 
     <view class="test-section">
       <text class="section-title">测试说明:</text>
-      <text class="description">测试uni.request方法GET请求时，参数填写重复是否会去重</text>
-      <text class="description">预期：安卓平台应该去重，其他平台行为待验证</text>
+      <text class="description">测试uni.request方法GET请求时，URL和data中同名参数的处理</text>
+      <text class="description">URL中有param=example，data中也有param:'example'</text>
+      <text class="description">预期：安卓平台应该去重，鸿蒙平台可能未去重</text>
     </view>
 
     <view class="buttons">
-      <button @click="testNormalRequest" class="test-btn">正常请求</button>
-      <button @click="testDuplicateParams" class="test-btn">重复参数请求</button>
-      <button @click="testDuplicateParamsInUrl" class="test-btn">URL中重复参数</button>
+      <button @click="testDuplicateParams" class="test-btn">测试重复参数</button>
+    </view>
+
+    <view class="buttons">
+      <button @click="testDuplicateUrlParams" class="test-btn">测试重复url参数</button>
+    </view>
+
+    <view class="buttons">
+      <button @click="testDuplicateUrlandDataParams" class="test-btn">
+        测试重复url参数和 data 参数
+      </button>
+    </view>
+
+    <view class="buttons">
+      <button @click="testOtherParams" class="test-btn">测试其他参数</button>
     </view>
 
     <view class="result">
@@ -26,83 +39,76 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 
-// 定义响应数据类型
-interface HttpBinResponse {
-  data: {
-    args: Record<string, any>
-    headers: Record<string, string>
-    url: string
-  }
-}
-
 const resultText = ref('点击按钮开始测试...')
 
-// 正常请求测试
-const testNormalRequest = () => {
-  resultText.value = '正常请求测试中...'
-
-  uni.request({
-    url: 'https://httpbin.org/get',
-    method: 'GET',
-    data: {
-      param1: 'value1',
-      param2: 'value2'
-    },
-    success: res => {
-      console.log('正常请求结果：', res)
-      const response = res as unknown as HttpBinResponse
-      resultText.value = `正常请求成功：${JSON.stringify(response.data.args, null, 2)}`
-    },
-    fail: err => {
-      console.error('正常请求失败：', err)
-      resultText.value = `正常请求失败：${err.errMsg}`
-    }
-  })
-}
-
-// 重复参数请求测试（通过data对象）
+// 重复参数测试
 const testDuplicateParams = () => {
-  resultText.value = '重复参数请求测试中...'
-
-  // 创建一个对象来测试重复参数的行为
-  const testData: Record<string, any> = {
-    param1: 'normal'
-  }
-  // 故意设置重复的参数来测试去重行为
-  testData['test'] = 'value1'
-  testData['test'] = 'value2' // 这会覆盖前面的值
+  resultText.value = '测试中...'
 
   uni.request({
-    url: 'https://httpbin.org/get',
     method: 'GET',
-    data: testData,
+    url: 'http://192.168.31.172:3000?param=example',
+    data: { param: 'example' },
     success: res => {
-      console.log('重复参数请求结果：', res)
-      const response = res as unknown as HttpBinResponse
-      resultText.value = `重复参数请求成功：${JSON.stringify(response.data.args, null, 2)}`
+      console.log('请求结果：', res)
+      resultText.value = `请求成功：${JSON.stringify(res.data, null, 2)}`
     },
     fail: err => {
-      console.error('重复参数请求失败：', err)
-      resultText.value = `重复参数请求失败：${err.errMsg}`
+      console.error('请求失败：', err)
+      resultText.value = `请求失败：${err.errMsg}`
     }
   })
 }
 
-// URL中重复参数测试
-const testDuplicateParamsInUrl = () => {
-  resultText.value = 'URL重复参数测试中...'
+const testDuplicateUrlParams = () => {
+  resultText.value = '测试中...'
 
   uni.request({
-    url: 'https://httpbin.org/get?test=value1&test=value2&param1=normal',
+    url: 'http://192.168.31.172:3000?param=example&param=example',
     method: 'GET',
     success: res => {
-      console.log('URL重复参数结果：', res)
-      const response = res as unknown as HttpBinResponse
-      resultText.value = `URL重复参数成功：${JSON.stringify(response.data.args, null, 2)}`
+      console.log('请求结果：', res)
+      resultText.value = `请求成功：${JSON.stringify(res.data, null, 2)}`
     },
     fail: err => {
-      console.error('URL重复参数失败：', err)
-      resultText.value = `URL重复参数失败：${err.errMsg}`
+      console.error('请求失败：', err)
+      resultText.value = `请求失败：${err.errMsg}`
+    }
+  })
+}
+
+const testDuplicateUrlandDataParams = () => {
+  resultText.value = '测试中...'
+
+  uni.request({
+    url: 'http://192.168.31.172:3000?param=example&param=example',
+    data: { param: 'example' },
+    method: 'GET',
+    success: res => {
+      console.log('请求结果：', res)
+      resultText.value = `请求成功：${JSON.stringify(res.data, null, 2)}`
+    },
+    fail: err => {
+      console.error('请求失败：', err)
+      resultText.value = `请求失败：${err.errMsg}`
+    }
+  })
+}
+
+const testOtherParams = () => {
+  resultText.value = '测试中...'
+
+  uni.request({
+    url: 'http://192.168.31.172:3000?param=example&param=example&param2=other123&parm=#1',
+    data: { param: 'example', param2: 'other' },
+    method: 'GET',
+    success: res => {
+      console.log('请求结果：', res)
+      resultText.value = `请求成功：${JSON.stringify(res.data, null, 2)}`
+    },
+    fail: err => {
+      console.error('请求失败：', err)
+      resultText.value = `请求失败：${err.errMsg}`
     }
   })
 }
