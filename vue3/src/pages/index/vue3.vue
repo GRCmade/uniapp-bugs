44c2c8c057b1922860949ca8d30c945c90b9fb1d
愈合 <1641284022@qq.com>
2025-07-09 15:38:24 +0800

    harmony-request-abort

--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,49 @@
 <template>
   <view>
-    <button @click="click">button</button>
+    <view>状态: {{ status }}</view>
+    <button @click="startRequest">发送请求</button>
+    <button @click="abortRequest" :disabled="!requestTask">中止请求</button>
   </view>
 </template>
 
 <script lang="ts" setup>
-const click = () => {}
+import { ref } from 'vue'
+
+const status = ref('未开始')
+let requestTask: any = null
+
+const startRequest = () => {
+  status.value = '请求中...'
+  requestTask = uni.request({
+    url: 'http://192.168.31.172:3001/set-timeout', //仅为示例，并非真实接口地址。
+    method: 'GET',
+    success: res => {
+      console.log('success', res)
+      status.value = '请求成功'
+      requestTask = null
+    },
+    fail: res => {
+      console.log('fail', res)
+      // 检查是否是因为 abort 导致的失败
+      if (status.value == '正在中止请求...') {
+        status.value = '请求已中止'
+      } else {
+        status.value = '请求失败'
+      }
+      requestTask = null
+    },
+    complete: () => {
+      console.log('complete')
+    }
+  })
+}
+
+const abortRequest = () => {
+  if (requestTask) {
+    requestTask.abort()
+    status.value = '正在中止请求...'
+  }
+}
 </script>
 
 <style></style>
