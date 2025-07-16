9d4a3d23c2a4220650b64230cc34e1bf150a34d9
愈合 <1641284022@qq.com>
2025-07-16 19:48:21 +0800

    harmony-onMounted-getSystemInfo

--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,29 @@
 <template>
-  <view>
-    <button @click="click">button</button>
-  </view>
+	<view>
+		<button @click="click">button</button>
+	</view>
 </template>
-
 <script lang="ts" setup>
-const click = () => {}
+	import { onMounted, nextTick } from 'vue'
+	onMounted(() => {
+		console.log("onMounted")
+			uni.getSystemInfo({
+				success: res => {
+					console.log(`windowHeight: ${JSON.stringify(res.windowHeight)}`)
+					console.log(`screenHeight: ${JSON.stringify(res.screenHeight)}`)
+					// console.log(`navigationBarHeight: ${JSON.stringify(res.navigationBarHeight)}`)
+				}
+			})
+			console.log("onMounted-1")
+	})
+	const click = () => {
+		uni.getSystemInfo({
+			success: res => {
+				console.log(`windowHeight: ${JSON.stringify(res.windowHeight)}`)
+				console.log(`screenHeight: ${JSON.stringify(res.screenHeight)}`)
+				// console.log(`navigationBarHeight: ${JSON.stringify(res.navigationBarHeight)}`)
+			}
+		})
+	}
 </script>
-
 <style></style>
