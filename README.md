7dbf92e151ebd12530dd041fb015659a8a26ce1d
愈合 <1641284022@qq.com>
2025-07-16 11:39:33 +0800

    block-pull-down-refresh

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 ======== README.md ========
 
-# 标题
+# 拖拽块上拉刷新
+
+一整个块上拉加载更多
+
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250716113849783.png)
 
 ======== END ========
--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -1,23 +1,27 @@
 <template>
 	<view>
 		<button @click="handleClick">Click me</button>
+		<uni-popup ref="popup" type="center">
+			<view class="pop">
+				底部弹出 Popup 自定义圆角
+			</view>
+		</uni-popup>
 	</view>
 </template>
-
 <script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
+	export default {
+		data() {
+			return {
+				message: 'Hello, World!'
+			}
+		},
+		methods: {
+			handleClick() {
+				console.log('Button clicked')
+				this.$refs.popup.open()
+			}
 		}
 	}
-}
 </script>
-
 <style scoped>
 </style>
