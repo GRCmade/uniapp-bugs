cd742c28b654d9477e098c3890a2d6e757494fd0
愈合 <1641284022@qq.com>
2025-07-30 11:14:51 +0800

    h5-movable-view-x-bug

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ======== README.md ========
 
-# 标题
+# h5 上 movable 不会回到开始点
+https://ask.dcloud.net.cn/question/212162
 
+暂时判断是写法存在问题 vue3.vue 中的是正常的
 ======== END ========
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,42 @@
 <template>
   <view>
-    <button @click="click">button</button>
+    <movable-area style="border: 1px solid red; width: 100%; height: 60rpx; margin-top: 30rpx">
+      <movable-view :x="x" direction="horizontal" @change="onChange" @touchmove="touchmove" @touchend="endTouch">text</movable-view>
+    </movable-area>
+    {{ x }}
+    <button @click="click">点击</button>
   </view>
 </template>
 
-<script lang="ts" setup>
-const click = () => {}
+<script setup>
+import { ref, nextTick } from 'vue'
+
+const x = ref(20)
+const old_x = ref(20)
+const onChange = e => {
+  old_x.value = e.detail.x
+}
+
+const endTouch = e => {
+  console.log('endTouch', e)
+	setTimeout(()=>{
+		x.value = old_x.value
+		nextTick(() => {
+		  x.value = 20
+		})
+	},100)
+}
+
+const touchmove = e => {
+  // console.log('touchmove', e)
+}
+
+const click = () => {
+  x.value = old_x.value
+  nextTick(() => {
+    x.value = 20
+  })
+}
 </script>
 
 <style></style>
