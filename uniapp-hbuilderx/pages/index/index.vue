f4e89f65542740e89484536bce02e211ee377fed
愈合 <1641284022@qq.com>
2025-07-31 17:14:32 +0800

    harmony-screen-brightness

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -1,23 +1,66 @@
 <template>
 	<view>
-		<button @click="handleClick">Click me</button>
+		<view class=""> slider : {{slider}}
+		</view>
+		<slider :value="slider" @change="sliderChange" step="1" />
+		<button @click="setScreenBrightness(20)">setScreenBrightness 20</button>
+		<button @click="setScreenBrightness(-10)">setScreenBrightness -10</button>
+		<button @click="setScreenBrightness(1999)">setScreenBrightness 1999</button>
+		<button @click="getScreenBrightness">getScreenBrightness</button>
+		<button @click="setKeepScreenOn(true)">setKeepScreenOn true</button>
+		<button @click="setKeepScreenOn(false)">setKeepScreenOn false</button>
 	</view>
 </template>
+<script lang="ts" setup>
+	import { ref } from 'vue'
 
-<script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
-		}
+	const setScreenBrightness = (value : number) => {
+		console.log("click")
+		slider.value = value
+		uni.setScreenBrightness({
+			value: value / 100,
+			success: function () {
+				console.log('success')
+			},
+			fail(res) {
+				console.log('fail', res)
+			}
+		})
 	}
-}
-</script>
+	const getScreenBrightness = () => {
+		uni.getScreenBrightness({
+			success: function (res) {
+				console.log('屏幕亮度值：' + res.value)
+				uni.showToast({
+					title: '屏幕亮度值：' + res.value
+				})
+			},
+			fail() {
+				console.log('fail')
+			}
+		})
+	}
 
-<style scoped>
-</style>
+	const slider = ref(60)
+
+	const sliderChange = (e) => {
+		slider.value = e.detail.value
+		setScreenBrightness(slider.value)
+		console.log('value 发生变化：' + e.detail.value)
+	}
+
+	setScreenBrightness(slider.value)
+
+	const setKeepScreenOn = (value : Boolean) => {
+		uni.setKeepScreenOn({
+			keepScreenOn:value,
+			success(res) {
+				console.log("res", res)
+			},
+			fail() {
+				console.log('fail')
+			},
+		})
+	}
+</script>
+<style></style>
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,31 @@
 <template>
   <view>
-    <button @click="click">button</button>
+    <button @click="setScreenBrightness">setScreenBrightness</button>
+    <button @click="getScreenBrightness">getScreenBrightness</button>
   </view>
 </template>
-
 <script lang="ts" setup>
-const click = () => {}
+const setScreenBrightness = () => {
+	console.log("click")
+  uni.setScreenBrightness({
+    value: 0.6,
+    success: function () {
+      console.log('success')
+    },
+		fail() {
+			console.log('fail')
+		}
+  })
+}
+const getScreenBrightness = () => {
+  uni.getScreenBrightness({
+    success: function (res) {
+      console.log('屏幕亮度值：' + res.value)
+    },
+		fail() {
+			console.log('fail')
+		}
+  })
+}
 </script>
-
 <style></style>
