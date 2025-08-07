273298d12128862ad1395f9c4bb1a7c0b0b0dbb0
愈合 <1641284022@qq.com>
2025-08-07 14:53:45 +0800

    harmony-webview-devtool-unwork

--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -5,7 +5,21 @@
 </template>
 
 <script lang="ts" setup>
-const click = () => {}
+import { onShow } from '@dcloudio/uni-app'
+onShow(() => {
+  console.log('页面显示')
+})
+const click = () => {
+  uni.chooseImage({
+    count: 1,
+    success: res => {
+      console.log('选择的图片:', res.tempFilePaths)
+    },
+    fail: err => {
+      console.error('选择图片失败:', err)
+    }
+  })
+}
 </script>
 
 <style></style>
