aef3981c6e87e968abbf5579c2ee0e4f409d6022
愈合 <1641284022@qq.com>
2025-08-08 09:36:37 +0800

    save

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -3,21 +3,20 @@
 		<button @click="handleClick">Click me</button>
 	</view>
 </template>
-
 <script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
+	import { test } from "@/uni_modules/harmony-button"
+	export default {
+		data() {
+			return {
+				message: 'Hello, World!'
+			}
+		},
+		methods: {
+			handleClick() {
+				console.log('Button clicked')
+			}
 		}
 	}
-}
 </script>
-
 <style scoped>
 </style>
