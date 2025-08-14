a396964a7149826d07def1e0ad6e75b7724afee0
愈合 <1641284022@qq.com>
2025-08-14 17:25:47 +0800

    template-cli

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -1,23 +1,37 @@
 <template>
 	<view>
 		<button @click="handleClick">Click me</button>
+		<button @click="handleClick2">Click to webview</button>
+		<button @click="handleClick3">redirect to webview</button>
 	</view>
 </template>
-
 <script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
+	export default {
+		data() {
+			return {
+				message: 'Hello, World!'
+			}
+		},
+		methods: {
+			handleClick() {
+				console.log('Button clicked')
+				uni.navigateTo({
+					url: "/pages/temp/temp"
+				})
+			},
+			handleClick2() {
+				console.log('Button clicked')
+				uni.navigateTo({
+					url: "/pages/test/test"
+				})
+			},
+			handleClick3() {
+				uni.redirectTo({
+					url: "/pages/test/test"
+				})
+			}
 		}
 	}
-}
 </script>
-
 <style scoped>
 </style>
---
+++ uniapp-hbuilderx/pages/temp/temp.vue
@@ -0,0 +1,20 @@
+<template>
+	<view> temp <button @click="click">temp</button>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			click() {
+				uni.navigateTo({
+					url: "/pages/test/test"
+				})
+			}
+		}
+	}
+</script>
+<style>
+</style>
