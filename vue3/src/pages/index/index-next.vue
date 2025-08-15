b7b7086fc07204d6584457d75a713311e2e9d5b4
愈合 <1641284022@qq.com>
2025-08-15 20:02:26 +0800

    harmony-video-direction

--- vue3/src/pages/index/index-next.vue
+++ vue3/src/pages/index/index-next.vue
@@ -1,23 +1,63 @@
 <template>
-  <view>
-    当前页面： index-next
-    <button @click="click">index-next</button>
-  </view>
+	<view>
+		<view class="uni-padding-wrap uni-common-mt">
+			<view>
+				<video id="myVideo" :direction="90" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4"
+					@error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+			</view> index-next 页面
+		</view>
+	</view>
 </template>
-
 <script>
-export default {
-  data() {
-    return {}
-  },
-  methods: {
-    click() {
-      uni.navigateTo({
-        url: '/pages/index/index-next-next'
-      })
-    }
-  }
-}
+	export default {
+		data() {
+			return {
+				src: '',
+				danmuList: [{
+					text: '第 1s 出现的弹幕',
+					color: '#ff0000',
+					time: 1
+				}, {
+					text: '第 3s 出现的弹幕',
+					color: '#ff00ff',
+					time: 3
+				}],
+				danmuValue: ''
+			}
+		},
+		onReady: function(res) {
+			// #ifndef MP-ALIPAY
+			this.videoContext = uni.createVideoContext('myVideo')
+			// #endif
+		},
+		methods: {
+			nav() {
+				uni.navigateTo({
+					url: "/pages/index/index-next"
+				})
+			},
+			sendDanmu: function() {
+				this.videoContext.sendDanmu({
+					text: this.danmuValue,
+					color: this.getRandomColor()
+				})
+				this.danmuValue = ''
+			},
+			videoErrorCallback: function(e) {
+				uni.showModal({
+					content: e.target.errMsg,
+					showCancel: false
+				})
+			},
+			getRandomColor: function() {
+				const rgb = []
+				for (let i = 0; i < 3; ++i) {
+					let color = Math.floor(Math.random() * 256).toString(16)
+					color = color.length == 1 ? '0' + color : color
+					rgb.push(color)
+				}
+				return '#' + rgb.join('')
+			}
+		}
+	}
 </script>
-
-<style></style>
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,63 @@
 <template>
-  <view>
-    <button @click="click">button</button>
-  </view>
+	<view>
+		<view class="uni-padding-wrap uni-common-mt">
+			<view>
+				<video id="myVideo" :direction="-90" src="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/2minute-demo.mp4"
+					@error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+			</view> index 首页面 <button @click="nav">index-next</button>
+		</view>
+	</view>
 </template>
-
-<script lang="ts" setup>
-const click = () => {}
+<script>
+	export default {
+		data() {
+			return {
+				src: '',
+				danmuList: [{
+					text: '第 1s 出现的弹幕',
+					color: '#ff0000',
+					time: 1
+				}, {
+					text: '第 3s 出现的弹幕',
+					color: '#ff00ff',
+					time: 3
+				}],
+				danmuValue: ''
+			}
+		},
+		onReady: function(res) {
+			// #ifndef MP-ALIPAY
+			this.videoContext = uni.createVideoContext('myVideo')
+			// #endif
+		},
+		methods: {
+			nav() {
+				uni.navigateTo({
+					url: "/pages/index/index-next"
+				})
+			},
+			sendDanmu: function() {
+				this.videoContext.sendDanmu({
+					text: this.danmuValue,
+					color: this.getRandomColor()
+				})
+				this.danmuValue = ''
+			},
+			videoErrorCallback: function(e) {
+				uni.showModal({
+					content: e.target.errMsg,
+					showCancel: false
+				})
+			},
+			getRandomColor: function() {
+				const rgb = []
+				for (let i = 0; i < 3; ++i) {
+					let color = Math.floor(Math.random() * 256).toString(16)
+					color = color.length == 1 ? '0' + color : color
+					rgb.push(color)
+				}
+				return '#' + rgb.join('')
+			}
+		}
+	}
 </script>
-
-<style></style>
