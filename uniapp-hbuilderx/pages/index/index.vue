b15b96e0c24973813599e05cf7d43ed7a2d76ded
愈合 <1641284022@qq.com>
2025-08-26 14:59:05 +0800

    ext-uni-data-picker

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -1,23 +1,21 @@
 <template>
 	<view>
-		<button @click="handleClick">Click me</button>
+		<uni-data-picker placeholder="请选择地址" popup-title="请选择城市" collection="opendb-city-china"
+			field="code as value, name as text" orderby="value asc" :step-searh="true" self-field="code"
+			parent-field="parent_code" @change="onchange" @nodeclick="onnodeclick">
+		</uni-data-picker>
 	</view>
 </template>
-
 <script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
+	export default {
+		data() {
+			return {}
+		},
+		methods: {
+			onchange(e) {
+				const value = e.detail.value
+			},
+			onnodeclick(node) {}
 		}
 	}
-}
 </script>
-
-<style scoped>
-</style>
