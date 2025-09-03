876f4d280d7566a5c5a5bffcb3ab018f1620efed
愈合 <1641284022@qq.com>
2025-09-03 15:29:19 +0800

    harmony-textarea-adjust-position

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 ======== README.md ========
 
-# 标题
+# 鸿蒙 textarea 中设置 showConfirmBar 造成
+
+![](https://yuhepicgo.oss-cn-beijing.aliyuncs.com/20250903151335218.png)
+
+会报错
+```
+I     Uncaught TypeError: currentWebview.setStyle is not a function
+I     [CONSOLE:14622] "Uncaught TypeError: currentWebview.setStyle is not a function", source: file:///data/storage/el1/bundle/entry/resources/resfile/apps/HBuilder/www/uni-app-view.umd.js (14622)
+W     adjustResize is deprecated.
+```
+
+导致了 webview 之后的逻辑不执行了
+
+导致页面还是会推到上方
 
 ======== END ========
--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,11 +1,69 @@
 <template>
-  <view>
-    <button @click="click">button</button>
-  </view>
+	<view class="rc-input-container">
+		<view class="rc-input-text">
+			<textarea class="rc-input-text-input" auto-height confirm-hold v-model="text" :maxlength="-1"
+				:adjust-position="false" @confirm="sendMessage" @input="inputHandler" :cursor-spacing="20" always-embed
+				:show-confirm-bar="false" :cursor="inputCursor" :focus="inputFocus" @blur="onBlurHandler"
+				@focus="onFocusHandler" :disable-default-padding="true" @keyboardheightchange="onKeyboardHeightChangeHandler"
+				confirm-type="send">
+      </textarea>
+		</view>
+	</view>
 </template>
-
-<script lang="ts" setup>
-const click = () => {}
+<script>
+	export default {
+		data() {
+			return {
+				text: '11',
+				inputCursor: 0,
+				inputFocus: false,
+			}
+		},
+		onLoad() {
+			// uniapp 生命周期
+		},
+		methods: {
+			sendMessage() {
+				console.info('----sendMessage----===------')
+			},
+			inputHandler(e) {
+				console.info('----inputHandler----===------')
+				this.text = e.detail.value
+			},
+			onBlurHandler() {
+				console.info('----onBlurHandler----===------')
+			},
+			onFocusHandler() {
+				console.info('----onFocusHandler----===------')
+			},
+			onKeyboardHeightChangeHandler(e) {
+				console.info('----onKeyboardHeightChangeHandler----===------', e)
+			}
+		}
+	}
 </script>
+<style lang="scss">
+	.rc-input-container {
+		z-index: 100;
+		position: fixed;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		box-sizing: border-box;
+		padding: 9px 11px 9px;
+		padding-bottom: calc(env(safe-area-inset-bottom));
+		padding-bottom: calc(constant(safe-area-inset-bottom));
+	}
 
-<style></style>
+	.rc-input-text-input {
+		background: #CCCCCC;
+		border-radius: 6px;
+		border: 1px solid #000000;
+		color: #FFFFFF;
+		padding: 7px 10px;
+		line-height: 22px;
+		min-height: 22px;
+		overflow-y: auto;
+		width: calc(100% - 20px);
+	}
+</style>
