4538534ddeee86152f2c1f1a4364da1f51452548
愈合 <1641284022@qq.com>
2025-09-04 20:38:44 +0800

    Revert "template-cli"
    This reverts commit a2ad0dab955c159c62802c31fcc4fe0054828a5e.

--- vue3/src/pages/index/vue3.vue
+++ vue3/src/pages/index/vue3.vue
@@ -1,55 +1,11 @@
 <template>
-	<view>
-		{{tempFilePath}}
-		<image :src="tempFilePath" mode=""></image>
-		<button @click="click">chooseMedia</button>
-		<button @click="chooseImage">chooseImage</button>
-	</view>
+  <view>
+    <button @click="click">button</button>
+  </view>
 </template>
-<script lang="ts" setup>
-	import { ref } from 'vue';
-	const tempFilePath = ref()
-	const fileSystemManager = uni.getFileSystemManager()
-	const click = () => {
-		uni.chooseMedia({
-			count: 1,
-			mediaType: ['image'],
-			sourceType: ['album', 'camera'],
-			success: (res) => {
-				console.log("chooseMedia", JSON.stringify(res))
-				tempFilePath.value = res.tempFiles[0].tempFilePath;
-				try {
-					const result = fileSystemManager.readFileSync(tempFilePath.value);
-					console.log("readFileSync - result", JSON.stringify(result))
-				} catch (err) {
-					console.log('readFileSync', err)  //  
-				}
-			},
-			fail: (err) => {
-				console.log('chooseMedia', err);
-			}
-		})
-	}
 
-	const chooseImage = () => {
-		uni.chooseImage({
-			count: 1,
-			mediaType: ['image'],
-			sourceType: ['album', 'camera'],
-			success: (res) => {
-				console.log("chooseImage", JSON.stringify(res))
-				tempFilePath.value = res.tempFiles[0].path;
-				try {
-					const result = fileSystemManager.readFileSync(tempFilePath.value);
-					console.log("readFileSync - result", JSON.stringify(result))
-				} catch (err) {
-					console.log('readFileSync', JSON.stringify(err))  //  
-				}
-			},
-			fail: (err) => {
-				console.log('chooseImage', err);
-			}
-		})
-	}
+<script lang="ts" setup>
+const click = () => {}
 </script>
+
 <style></style>
