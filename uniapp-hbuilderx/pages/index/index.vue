aa09508cff89b85c28c98a941be83c68fdc576e7
愈合 <1641284022@qq.com>
2025-09-17 12:03:49 +0800

    harmony-getContext-getHostContext

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -3,21 +3,21 @@
 		<button @click="handleClick">Click me</button>
 	</view>
 </template>
-
 <script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
+	import { settingGetValue } from "@/uni_modules/harmony-button"
+	export default {
+		data() {
+			return {
+				message: 'Hello, World!'
+			}
+		},
+		methods: {
+			handleClick() {
+				console.log('Button clicked')
+				settingGetValue()
+			}
 		}
 	}
-}
 </script>
-
 <style scoped>
 </style>
