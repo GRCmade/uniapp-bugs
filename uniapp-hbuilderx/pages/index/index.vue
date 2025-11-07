4fea17a1410bb2b98c6169a9f1d33c5246b0abf6
愈合 <1641284022@qq.com>
2025-11-07 15:04:03 +0800

    app-slider-value-float

--- uniapp-hbuilderx/pages/index/index.vue
+++ uniapp-hbuilderx/pages/index/index.vue
@@ -1,22 +1,19 @@
 <template>
 	<view>
+		<slider min="0.3" max="3" :value="0.3" step="0.05" activeColor="#61bc55" @change="sliderChange"
+			@changing="sliderChange" show-value />
 		<button @click="handleClick">Click me</button>
 	</view>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			message: 'Hello, World!'
-		}
-	},
-	methods: {
-		handleClick() {
-			console.log('Button clicked')
+	export default {
+		methods: {
+			sliderChange(e) {
+				console.log('value 发生变化：' + e.detail.value)
+			}
 		}
 	}
-}
 </script>
 
 <style scoped>
